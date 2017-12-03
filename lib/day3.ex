8d50a20f196b1bb84deb12c0f7b38e54b8ad003b
Jaakko Husso <jaakko.husso@futurice.com>
2017-12-03 15:53:39 +0200

    Initial solution to day 3

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,27 +1,228 @@
+defmodule Day3.Cell do
+  @moduledoc """
+  Documentation for a Cell in matrix
+  """
+  alias Day3.Cell
+
+  defstruct x: 0, y: 0, v: nil, going: nil, sum: nil
+
+  def add(%Cell{} = a, %Cell{} = b) do
+    %Cell{x: a.x + b.x, y: a.y + b.y}
+  end
+
+  def add(%Cell{} = a, %Cell{} = b, v, going) do
+    %Cell{x: a.x + b.x, y: a.y + b.y, v: v, going: going}
+  end
+end
+
+defmodule Day3.Matrix do
+  @moduledoc """
+  Documentation for Matrix which stores Cells
+  """
+
+  alias Day3.Cell, as: Cell
+  require Logger
+
+  @doc """
+  Creates a Matrix
+
+  ## Examples:
+    iex> Matrix.create(5)
+    [%Cell{going: :south, v: 5, x: -1, y: 1},
+     %Cell{going: :west, v: 4, x: 0, y: 1},
+     %Cell{going: :west, v: 3, x: 1, y: 1},
+     %Cell{going: :north, v: 2, x: 1, y: 0},
+     %Cell{going: :east, v: 1, x: 0, y: 0}]
+  """
+  def create(max_v) do
+    first = %Cell{x: 0, y: 0, v: 1, going: :east}
+
+    create_cells([first], max_v)
+  end
+
+  defp create_cells([%Cell{v: v} | _tail] = cells, max_v) when v == max_v do
+    cells
+  end
+
+  defp create_cells([%Cell{} = previous | _tail] = cells, max_v) do
+    create_cells([next_cell(previous) | cells], max_v)
+  end
+
+  @doc """
+  Creates a Matrix with neighbour values sum as value
+
+  ## Examples:
+    iex> Matrix.create_with_neighbours(5)
+    [%Cell{going: :south, sum: 5, v: 5, x: -1, y: 1},
+     %Cell{going: :west, sum: 4, v: 4, x: 0, y: 1},
+     %Cell{going: :west, sum: 2, v: 3, x: 1, y: 1},
+     %Cell{going: :north, sum: 1, v: 2, x: 1, y: 0},
+     %Cell{going: :east, sum: 1, v: 1, x: 0, y: 0}]
+  """
+  def create_with_neighbours(max_v) do
+    first = %Cell{x: 0, y: 0, v: 1, going: :east, sum: 1}
+
+    create_cells_with_neighbours([first], max_v)
+  end
+
+  defp create_cells_with_neighbours([%Cell{v: v} | _tail] = cells, max_v) when v == max_v do
+    cells
+  end
+
+  defp create_cells_with_neighbours([%Cell{} = previous | _tail] = cells, max_v) do
+    next = next_cell(previous)
+    sum = find_neighbours_values(cells, next)
+
+    create_cells_with_neighbours([%{next | sum: sum} | cells], max_v)
+  end
+
+  @doc """
+  Finds next Cell and the direction it is 'going' to, forming a counter-clockwise spiral
+
+  ## Examples:
+    iex> Matrix.next_cell(%Cell{v: 1, x: 0, y: 0, going: :east})
+    %Cell{v: 2, x: 1, y: 0, going: :north}
+    iex> Matrix.next_cell(%Cell{v: 2, x: 1, y: 0, going: :north})
+    %Cell{v: 3, x: 1, y: 1, going: :west}
+    iex> Matrix.next_cell(%Cell{v: 3, x: 1, y: 1, going: :west})
+    %Cell{v: 4, x: 0, y: 1, going: :west}
+    iex> Matrix.next_cell(%Cell{v: 4, x: 0, y: 1, going: :west})
+    %Cell{v: 5, x: -1, y: 1, going: :south}
+    iex> Matrix.next_cell(%Cell{v: 5, x: -1, y: 1, going: :south})
+    %Cell{v: 6, x: -1, y: 0, going: :south}
+    iex> Matrix.next_cell(%Cell{v: 6, x: -1, y: 0, going: :south})
+    %Cell{v: 7, x: -1, y: -1, going: :east}
+    iex> Matrix.next_cell(%Cell{v: 7, x: -1, y: -1, going: :east})
+    %Cell{v: 8, x: 0, y: -1, going: :east}
+    iex> Matrix.next_cell(%Cell{v: 8, x: 0, y: -1, going: :east})
+    %Cell{v: 9, x: 1, y: -1, going: :east}
+    iex> Matrix.next_cell(%Cell{v: 9, x: 1, y: -1, going: :east})
+    %Cell{v: 10, x: 2, y: -1, going: :north}
+  """
+  def next_cell(%Cell{v: prev_v, going: going} = previous) do
+    prev_w = width(prev_v)
+    prev_h = height(prev_v)
+
+    next_v = prev_v + 1
+    
+    next_w = width(next_v)
+    next_h = height(next_v)
+
+    case going do
+      :east ->
+        if prev_w < next_w do
+          Cell.add(previous, directions(:east), next_v, :north)
+        else
+          Cell.add(previous, directions(:east), next_v, :east)
+        end
+      :west ->
+        if prev_w < next_w do
+          Cell.add(previous, directions(:west), next_v, :south)
+        else
+          Cell.add(previous, directions(:west), next_v, :west)
+        end
+      :north ->
+        if prev_h < next_h do
+          Cell.add(previous, directions(:north), next_v, :west)
+        else
+          Cell.add(previous, directions(:north), next_v, :north)
+        end
+      :south ->
+        if prev_h < next_h do
+          Cell.add(previous, directions(:south), next_v, :east)
+        else
+          Cell.add(previous, directions(:south), next_v, :south)
+        end
+    end
+  end
+
+  # Returns a vector representing movement to direction
+  defp directions(direction) do
+    case direction do
+      :west       -> %Cell{x: -1, y: 0}
+      :east       -> %Cell{x: 1, y: 0}
+      :north      -> %Cell{x: 0, y: 1}
+      :south      -> %Cell{x: 0, y: -1}
+      :northeast  -> %Cell{x: 1, y: 1}
+      :southeast  -> %Cell{x: 1, y: -1}
+      :southwest  -> %Cell{x: -1, y: -1}
+      :northwest  -> %Cell{x: -1, y: 1}
+    end
+  end
+
+  # Finds the required width of grid to draw the spiral ending to number
+  defp width(number) do
+    round(Float.ceil(:math.sqrt(number)))
+  end
+
+  # Finds the required height of grid to draw the spiral ending to number
+  defp height(number) do
+    round(:math.sqrt(number))
+  end
+
+  @doc """
+  Finds all neighbours that currently exist for cell in cells
+  """
+  def find_neighbours_values(cells, %Cell{} = cell) do
+    potential_neighbours = [
+      Cell.add(cell, directions(:north)),
+      Cell.add(cell, directions(:northeast)),
+      Cell.add(cell, directions(:east)),
+      Cell.add(cell, directions(:southeast)),
+      Cell.add(cell, directions(:south)),
+      Cell.add(cell, directions(:southwest)),
+      Cell.add(cell, directions(:west)),
+      Cell.add(cell, directions(:northwest))
+    ]
+
+    potential_neighbours
+    |> Enum.map(fn(neighbour) ->
+        Enum.find(cells, fn(c) -> 
+          neighbour.x == c.x && neighbour.y == c.y
+        end)
+      end)
+    |> Enum.reduce(0, fn(x, acc) ->
+      if is_nil(x), do: acc, else: acc + x.sum
+    end)
+  end
+end
+
 defmodule Day3.Part1 do
   @moduledoc """
   Documentation for Part1.
   """
 
+  alias Day3.Matrix, as: Matrix
+
   @doc """
-  Solves the spreadsheet checksum with min-max values
+  Solves the distance from input to middle
+
+  36  35  34  33  32  31
+  17  16  15  14  13  30
+  18   5   4   3  12  29
+  19   6   1   2  11  28
+  20   7   8   9  10  27
+  21  22  23  24  25  26
 
   ## Examples:
-    iex> Part1.solve("5,1,9,5;7,5,3;2,4,6,8")
-    18
+    iex> Part1.solve(1)
+    0
+    iex> Part1.solve(12)
+    3
+    iex> Part1.solve(23)
+    2
+    iex> Part1.solve(1024)
+    31
   """
   def solve(input) do
     input
-    |> String.split(";", trim: true)
-    |> Enum.map(fn(x) -> String.split(x, ",", trim: true) end)
-    |> Enum.map(fn(x) -> checksum_row(x) end)
-    |> Enum.sum
+    |> Matrix.create
+    |> Enum.find(fn(x) -> x.v == input end)
+    |> distance
   end
 
-  defp checksum_row(row) do
-    row_as_integers = Enum.map(row, fn(x) -> String.to_integer(x) end)
-
-    Enum.max(row_as_integers) - Enum.min(row_as_integers)
+  defp distance(position) do
+    abs(position.x) + abs(position.y)
   end
 end
 
@@ -30,39 +231,48 @@ defmodule Day3.Part2 do
   Documentation for Part2.
   """
 
+  alias Day3.Matrix, as: Matrix
+  alias Day3.Cell, as: Cell
+  
   @doc """
-  Solves the spreadsheet checksum with evenly divisible values
+  Finds the first value written that is larger than puzzle input
+
+  147  142  133  122   59
+  304    5    4    2   57
+  330   10    1    1   54
+  351   11   23   25   26
+  362  747  806--->   ...
 
   ## Examples:
-    iex> Part2.solve("5,9,2,8;9,4,7,3;3,8,6,5")
-    9
+    iex> Part2.solve(1)
+    1
+    iex> Part2.solve(2)
+    1
+    iex> Part2.solve(3)
+    2
+    iex> Part2.solve(4)
+    4
+    iex> Part2.solve(5)
+    5
+    iex> Part2.solve(6)
+    10
+    iex> Part2.solve(7)
+    11
+    iex> Part2.solve(8)
+    23
+    iex> Part2.solve(9)
+    25
+    iex> Part2.solve(10)
+    26
   """
   def solve(input) do
     input
-    |> String.split(";", trim: true)
-    |> Enum.map(fn(x) -> String.split(x, ",", trim: true) end)
-    |> Enum.map(fn(x) -> checksum_row(x) end)
-    |> Enum.sum
+    |> Matrix.create_with_neighbours
+    |> Enum.find(fn(x) -> x.v == input end)
+    |> pick_sum
   end
 
-  defp checksum_row(row) do
-    r = row
-    |> Enum.map(fn(x) -> String.to_integer(x) end)
-    |> Enum.with_index
-
-    r
-    |> Enum.reduce_while(0, fn(e, acc) -> find_even_division(e, acc, r) end)
-  end
-
-  defp find_even_division(e, acc, row) do
-    {value, index} = e
-
-    even = Enum.find(row, fn(x) -> index != elem(x, 1) && rem(value, elem(x, 0)) == 0 end)
-
-    if is_nil(even) do
-      {:cont, acc}
-    else
-      {:halt, div(value, elem(even, 0))}
-    end
+  defp pick_sum(%Cell{sum: sum}) do
+    sum
   end
 end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -3,7 +3,10 @@ defmodule Day3Test do
   
   alias Day3.Part1, as: Part1
   alias Day3.Part2, as: Part2
+  alias Day3.Matrix, as: Matrix
+  alias Day3.Cell, as: Cell
 
+  doctest Matrix
   doctest Part1
   doctest Part2
 end
