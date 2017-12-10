1e3caedf017f7a5f9a16d82051e617fee4b55acd
Jaakko Husso <jaakko.husso@futurice.com>
2017-12-10 15:48:12 +0200

    Add Day 10 Part two solution.

--- test/day10_test.exs
+++ test/day10_test.exs
@@ -1,9 +1,11 @@
-defmodule Day9Test do
+defmodule Day10Test do
   use ExUnit.Case
   
-  alias Day10.Part1, as: Part1
   alias Day10
+  alias Day10.Part1, as: Part1
+  alias Day10.Part2, as: Part2
   
-  doctest Part1
   doctest Day10
+  doctest Part1
+  doctest Part2
 end
