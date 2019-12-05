e8f06f2910ab705147c7990170ff46bea8a60948
Jaakko Husso <jaakko.husso@futurice.com>
2019-12-05 23:46:46 +0200

    Day 5 part 2 solutions

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,20 +1,20 @@
 const INPUT_FILE: &str = include_str!("../input.txt");
 
-fn read_param(program: &Vec<i32>, parameter: usize, mode: i32) -> i32 {
+fn read_param(program: &Vec<i32>, param: usize, mode: i32) -> i32 {
     match mode {
-        0 => program[parameter],
-        1 => parameter as i32,
+        0 => program[param],
+        1 => param as i32,
         _ => panic!("unknown mode")
     }
 }
 
 // Opcode 1 adds together numbers read from two positions and stores the result in a third position.
 fn add(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
-    let a_addr = program[program_counter + 1] as usize;
-    let b_addr = program[program_counter + 2] as usize;
-    let mem_addr = program[program_counter + 3] as usize;
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+    let param_2 = program[program_counter + 3] as usize;
 
-    program[mem_addr] = read_param(&program, a_addr, mode.0) + read_param(&program, b_addr, mode.1);
+    program[param_2] = read_param(&program, param_0, mode.0) + read_param(&program, param_1, mode.1);
     program_counter += 4;
 
     return (program, program_counter, registers);
@@ -22,11 +22,11 @@ fn add(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32),
 
 // Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
 fn mul(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
-    let a_addr = program[program_counter + 1] as usize;
-    let b_addr = program[program_counter + 2] as usize;
-    let mem_addr = program[program_counter + 3] as usize;
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+    let param_2 = program[program_counter + 3] as usize;
 
-    program[mem_addr] = read_param(&program, a_addr, mode.0) * read_param(&program, b_addr, mode.1);
+    program[param_2] = read_param(&program, param_0, mode.0) * read_param(&program, param_1, mode.1);
     program_counter += 4;
 
     return (program, program_counter, registers);
@@ -35,8 +35,8 @@ fn mul(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32),
 // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. 
 // For example, the instruction 3,50 would take an input value and store it at address 50.
 fn load_in(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), _mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
-    let mem_addr = program[program_counter + 1] as usize;
-    program[mem_addr] = registers.0;
+    let param_0 = program[program_counter + 1] as usize;
+    program[param_0] = registers.0;
     program_counter += 2;
 
     return (program, program_counter, registers);
@@ -45,9 +45,71 @@ fn load_in(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i
 // Opcode 4 outputs the value of its only parameter.
 // For example, the instruction 4,50 would output the value at address 50.
 fn load_out(program: Vec<i32>, mut program_counter: usize, mut registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
-    let mem_addr = program[program_counter + 1] as usize;
-    registers.1 = read_param(&program, mem_addr, mode.0);
+    let param_0 = program[program_counter + 1] as usize;
+    registers.1 = read_param(&program, param_0, mode.0);
     program_counter += 2;
+
+    return (program, program_counter, registers);
+}
+
+// Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+fn jump_if_true(program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+
+    if read_param(&program, param_0, mode.0) != 0 {
+        program_counter = read_param(&program, param_1, mode.1) as usize;
+    } else {
+        program_counter += 3;
+    }
+
+    return (program, program_counter, registers);
+}
+
+// Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+fn jump_if_false(program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+
+    if read_param(&program, param_0, mode.0) == 0 {
+        program_counter = read_param(&program, param_1, mode.1) as usize;
+    } else {
+        program_counter += 3;
+    }
+
+    return (program, program_counter, registers);
+}
+
+// Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+fn less_than(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+    let param_2 = program[program_counter + 3] as usize;
+
+    if read_param(&program, param_0, mode.0) < read_param(&program, param_1, mode.1) {
+        program[param_2] = 1;
+    } else {
+        program[param_2] = 0;
+    }
+
+    program_counter += 4;
+
+    return (program, program_counter, registers);
+}
+
+// Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+fn equals(mut program: Vec<i32>, mut program_counter: usize, registers: (i32, i32), mode: (i32, i32, i32)) -> (Vec<i32>, usize, (i32, i32)) {
+    let param_0 = program[program_counter + 1] as usize;
+    let param_1 = program[program_counter + 2] as usize;
+    let param_2 = program[program_counter + 3] as usize;
+
+    if read_param(&program, param_0, mode.0) == read_param(&program, param_1, mode.1) {
+        program[param_2] = 1;
+    } else {
+        program[param_2] = 0;
+    }
+
+    program_counter += 4;
 
     return (program, program_counter, registers);
 }
@@ -55,6 +117,9 @@ fn load_out(program: Vec<i32>, mut program_counter: usize, mut registers: (i32, 
 fn run_program(mut program: Vec<i32>, input: i32) -> (Vec<i32>, (i32, i32)) {
     let mut program_counter = 0;
     let mut registers = (input, 0);
+    
+    println!("[DEBUG]: input: {}", input);
+    println!("[DEBUG]: program: {:?}", program);
 
     loop {
         let instruction = program[program_counter];
@@ -74,6 +139,10 @@ fn run_program(mut program: Vec<i32>, input: i32) -> (Vec<i32>, (i32, i32)) {
             2 => mul(program, program_counter, registers, mode),
             3 => load_in(program, program_counter, registers, mode),
             4 => load_out(program, program_counter, registers, mode),
+            5 => jump_if_true(program, program_counter, registers, mode),
+            6 => jump_if_false(program, program_counter, registers, mode),
+            7 => less_than(program, program_counter, registers, mode),
+            8 => equals(program, program_counter, registers, mode),
             99 => break,
             _ => {
                 println!("[ERROR]: pc: {}, opcode: {}, program: {:?}", program_counter, program[program_counter], program);
@@ -83,10 +152,14 @@ fn run_program(mut program: Vec<i32>, input: i32) -> (Vec<i32>, (i32, i32)) {
         program = new_program;
         program_counter = new_program_counter;
         registers = new_registers;
+
+        println!("[DEBUG]: program: {:?}", program);
     }
 
     println!("[HALT]: registers: {:?}, pc: {}, opcode: {}, program: {:?}",
         registers, program_counter, program[program_counter], program);
+
+    println!("[DEBUG]: output: {}", registers.1);
 
     return (program, registers);
 }
@@ -99,11 +172,21 @@ fn part_1(main_program: &Vec<i32>) -> i32 {
     return registers.1;
 }
 
+fn part_2(main_program: &Vec<i32>) -> i32 {
+    let program = main_program.clone();
+
+    let (_halt_state, registers) = run_program(program, 5);
+
+    return registers.1;
+}
+
 fn main() -> () {
     let program: Vec<i32> = INPUT_FILE.split(',').map(|register| register.parse::<i32>()    .expect("Parse fail")).collect();
     let part1_program_result = part_1(&program);
+    let part2_program_result = part_2(&program);
 
     println!("Part 1: {}", part1_program_result);
+    println!("Part 2: {}", part2_program_result);
 }
 
 #[cfg(test)]
@@ -134,9 +217,69 @@ mod tests {
         assert_eq!(run_program(vec![1101,100,-1,4,0], 0), (vec![1101,100,-1,4,99], (0, 0)));
     }
 
-    // #[test]
-    // fn it_solves_part2() {
-    //     let program: Vec<i32> = INPUT_FILE.split(',').map(|register| register.parse::<i32>().expect("Parse fail")).collect();
-    //     assert_eq!(part_2(&program), 8444);
-    // }
+    #[test]
+    fn it_solves_part2_compare_examples() {
+        // Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        let (_program, output) = run_program(vec![3,9,8,9,10,9,4,9,99,-1,8], 8);
+        assert_eq!(output.1, 1);
+        let (_program, output) = run_program(vec![3,9,8,9,10,9,4,9,99,-1,8], 9);
+        assert_eq!(output.1, 0);
+
+        // Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        let (_program, output) = run_program(vec![3,9,7,9,10,9,4,9,99,-1,8], 7);
+        assert_eq!(output.1, 1);
+        let (_program, output) = run_program(vec![3,9,8,9,10,9,4,9,99,-1,8], 9);
+        assert_eq!(output.1, 0);
+
+        // Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        let (_program, output) = run_program(vec![3,3,1108,-1,8,3,4,3,99], 8);
+        assert_eq!(output.1, 1);
+        let (_program, output) = run_program(vec![3,3,1108,-1,8,3,4,3,99], 9);
+        assert_eq!(output.1, 0);
+
+        // Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not)
+        let (_program, output) = run_program(vec![3,3,1107,-1,8,3,4,3,99], 7);
+        assert_eq!(output.1, 1);
+        let (_program, output) = run_program(vec![3,3,1107,-1,8,3,4,3,99], 9);
+        assert_eq!(output.1, 0);
+    }
+
+    #[test]
+    fn it_solves_part2_jump_tests() {
+        // Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+
+        // using position mode
+        let (_program, output) = run_program(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0);
+        assert_eq!(output.1, 0);
+        let (_program, output) = run_program(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 1);
+        assert_eq!(output.1, 1);
+
+        // // using immediate mode
+        let (_program, output) = run_program(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0);
+        assert_eq!(output.1, 0);
+        let (_program, output) = run_program(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 123);
+        assert_eq!(output.1, 1);
+    }
+
+    #[test]
+    fn it_solves_part2_large_example() {
+        // The program uses an input instruction to ask for a single number.
+        // The program will then output 999 if the input value is below 8
+        let (_program, output) = run_program(vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 7);
+        assert_eq!(output.1, 999);
+
+        // output 1000 if the input value is equal to 8
+        let (_program, output) = run_program(vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 8);
+        assert_eq!(output.1, 1000);
+
+        // or output 1001 if the input value is greater than 8.
+        let (_program, output) = run_program(vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 9);
+        assert_eq!(output.1, 1001);
+    }
 }
