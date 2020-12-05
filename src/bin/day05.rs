c073ddd546ac339ed0309734726cce76efe61099
Jaakko Husso <jaakko.t.husso@gmail.com>
2020-12-05 13:53:00 +0200

    Implement day 05 part 2 using arithmetic sum and small cleanup

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,6 +1,13 @@
 const INPUT_FILE: &str = include_str!("../../inputs/day05.txt");
 
-fn parse_boarding_pass(input: &str) -> (usize, usize, usize) {
+#[derive(Debug, PartialEq)]
+struct BoardingPass {
+    row: u8,
+    column: u8,
+    id: usize
+}
+
+fn parse_boarding_pass(input: &str) -> BoardingPass {
     let binary_str: String = input
         .chars()
         .map(|c| match c {
@@ -10,46 +17,76 @@ fn parse_boarding_pass(input: &str) -> (usize, usize, usize) {
         })
         .collect();
 
-    let row = usize::from_str_radix(&binary_str[0..7], 2).unwrap();
-    let column = usize::from_str_radix(&binary_str[7..10], 2).unwrap();
+    let row = u8::from_str_radix(&binary_str[0..7], 2).unwrap();
+    let column = u8::from_str_radix(&binary_str[7..10], 2).unwrap();
 
-    (row, column, 8 * row + column)
+    BoardingPass {
+        row: row,
+        column: column,
+        id: 8 * row as usize + column as usize
+    }
 }
 
 fn part_1(input: &str) -> usize {
     let highest_seat = input
         .lines()
         .map(parse_boarding_pass)
-        .max_by(|a, b| a.2.cmp(&b.2))
+        .max_by(|a, b| a.id.cmp(&b.id))
         .unwrap();
 
-    highest_seat.2
+    highest_seat.id
 }
 
 fn part_2(input: &str) -> usize {
-    let mut seats: Vec<(usize, usize, usize)> = input
+    let mut seats: Vec<BoardingPass> = input
         .lines()
         .map(parse_boarding_pass)
         .collect();
 
-    seats.sort_by(|a, b| a.2.cmp(&b.2));
-    let first_id = seats[0].2;
+    seats.sort_by(|a, b| a.id.cmp(&b.id));
+    let first_id = seats[0].id;
 
     let (_, seat_higher_than_me) = seats
         .iter()
         .enumerate()
-        .find(|(index, seat)| seat.2 - first_id != *index)
+        .find(|(index, seat)| seat.id - first_id != *index)
         .unwrap();
 
-    seat_higher_than_me.2 - 1
+    seat_higher_than_me.id - 1
+}
+
+fn part_2_arithmetic_sum(input: &str) -> usize {
+    let seats: Vec<BoardingPass> = input
+        .lines()
+        .map(parse_boarding_pass)
+        .collect();
+
+    let highest_seat_by_id = seats
+        .iter()
+        .max_by(|a, b| a.id.cmp(&b.id))
+        .unwrap();
+
+    let lowest_seat_by_id = seats
+        .iter()
+        .min_by(|a, b| a.id.cmp(&b.id))
+        .unwrap();
+
+    // Arithmetic sum from n to m can be calculated with formula `n * (a_n + a_m) / 2`
+    // The missing seat ID will be the number we're missing from the sum if we sum up our seat IDs
+    let correct_sum = (seats.len() + 1) * (lowest_seat_by_id.id + highest_seat_by_id.id) / 2;
+    let actual_sum = seats.iter().fold(0, |acc, seat| acc + seat.id);
+
+    correct_sum - actual_sum
 }
 
 fn main() -> () {
     let part_1_result = part_1(INPUT_FILE);
     let part_2_result = part_2(INPUT_FILE);
+    let part_2_result_extra = part_2_arithmetic_sum(INPUT_FILE);
 
     println!("[INFO]: Part 1: {:?}", part_1_result);
     println!("[INFO]: Part 2: {:?}", part_2_result);
+    println!("[INFO]: Part 2+: {:?}", part_2_result_extra);
 }
 
 #[cfg(test)]
@@ -58,10 +95,10 @@ mod tests {
 
     #[test]
     fn it_parses_part1_examples() {
-        assert_eq!(parse_boarding_pass("FBFBBFFRLR"), (44, 5, 357));
-        assert_eq!(parse_boarding_pass("BFFFBBFRRR"), (70, 7, 567));
-        assert_eq!(parse_boarding_pass("FFFBBBFRRR"), (14, 7, 119));
-        assert_eq!(parse_boarding_pass("BBFFBBFRLL"), (102, 4, 820));
+        assert_eq!(parse_boarding_pass("FBFBBFFRLR"), BoardingPass{ row: 44, column: 5, id: 357 });
+        assert_eq!(parse_boarding_pass("BFFFBBFRRR"), BoardingPass{ row: 70, column: 7, id: 567 });
+        assert_eq!(parse_boarding_pass("FFFBBBFRRR"), BoardingPass{ row: 14, column: 7, id: 119 });
+        assert_eq!(parse_boarding_pass("BBFFBBFRLL"), BoardingPass{ row: 102, column: 4, id: 820 });
     }
 
     #[test]
