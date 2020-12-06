3a9ca95035c0c1dc685f767ea4350ba99b12cef9
Jaakko Husso <jaakko.t.husso@gmail.com>
2020-12-06 11:17:06 +0200

    Avoid copying the common characters so much by dropping intersect. Format code

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -2,39 +2,34 @@ use std::collections::HashSet;
 
 const INPUT_FILE: &str = include_str!("../../inputs/day06.txt");
 
-fn parse_answers_1(input: &str) -> HashSet::<char> {
-    let answers: HashSet::<char> = input.lines()
-        .flat_map(|line| line.chars())
-        .collect();
+fn parse_answers_1(input: &str) -> HashSet<char> {
+    let answers: HashSet<char> = input.lines().flat_map(|line| line.chars()).collect();
 
     answers
 }
 
-fn parse_answers_2(input: &str) -> HashSet::<char> {
-    let mut common_answers: HashSet::<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
-    
+fn parse_answers_2(input: &str) -> HashSet<char> {
+    let mut common_answers: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+
     for line in input.lines() {
-        let single_answers: HashSet::<char> = line.chars().collect();
-        common_answers = common_answers.intersection(&single_answers).copied().collect();
+        let mut single_answers: HashSet<char> = line.chars().collect();
+        common_answers = common_answers
+            .iter()
+            .filter_map(|v| single_answers.take(v))
+            .collect();
     }
 
     common_answers
 }
 
 fn part_1(input: &str) -> usize {
-    let sum = input
-        .split("\n\n")
-        .map(|i| parse_answers_1(i).len())
-        .sum();
+    let sum = input.split("\n\n").map(|i| parse_answers_1(i).len()).sum();
 
     sum
 }
 
 fn part_2(input: &str) -> usize {
-    let sum = input
-        .split("\n\n")
-        .map(|i| parse_answers_2(i).len())
-        .sum();
+    let sum = input.split("\n\n").map(|i| parse_answers_2(i).len()).sum();
 
     sum
 }
@@ -53,11 +48,26 @@ mod tests {
 
     #[test]
     fn it_parses_part1_examples() {
-        assert_eq!(parse_answers_1("abc"), vec!['a', 'b', 'c'].into_iter().collect());
-        assert_eq!(parse_answers_1("a\nb\nc"), vec!['a', 'b', 'c'].into_iter().collect());
-        assert_eq!(parse_answers_1("ab\nac"), vec!['a', 'b', 'c'].into_iter().collect());
-        assert_eq!(parse_answers_1("a\na\na\na"), vec!['a'].into_iter().collect());
-        assert_eq!(parse_answers_1("b"), vec!['b'].into_iter().collect());
+        assert_eq!(
+            parse_answers_1("abc"),
+            vec!['a', 'b', 'c'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_1("a\nb\nc"),
+            vec!['a', 'b', 'c'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_1("ab\nac"),
+            vec!['a', 'b', 'c'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_1("a\na\na\na"),
+            vec!['a'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_1("b"),
+            vec!['b'].into_iter().collect()
+        );
     }
 
     #[test]
@@ -86,11 +96,26 @@ mod tests {
 
     #[test]
     fn it_parses_part2_examples() {
-        assert_eq!(parse_answers_2("abc"), vec!['a', 'b', 'c'].into_iter().collect());
-        assert_eq!(parse_answers_2("a\nb\nc"), vec![].into_iter().collect());
-        assert_eq!(parse_answers_2("ab\nac"), vec!['a'].into_iter().collect());
-        assert_eq!(parse_answers_2("a\na\na\na"), vec!['a'].into_iter().collect());
-        assert_eq!(parse_answers_2("b"), vec!['b'].into_iter().collect());
+        assert_eq!(
+            parse_answers_2("abc"),
+            vec!['a', 'b', 'c'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_2("a\nb\nc"),
+            vec![].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_2("ab\nac"),
+            vec!['a'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_2("a\na\na\na"),
+            vec!['a'].into_iter().collect()
+        );
+        assert_eq!(
+            parse_answers_2("b"),
+            vec!['b'].into_iter().collect()
+        );
     }
 
     #[test]
