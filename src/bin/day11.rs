7067393d033e30ec2e3dafe470fafe03384fa7c5
Jaakko Husso <jaakko.t.husso@gmail.com>
2020-12-11 23:38:19 +0200

    Day 11 part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -40,6 +40,7 @@ impl GameOfSeats {
         }
     }
 
+    #[allow(dead_code)]
     fn print(&self) {
         println!("Step {:?}:", self.step);
         for y in 0..self.dimensions.1 {
@@ -51,15 +52,72 @@ impl GameOfSeats {
         print!("\n");
     }
 
+    fn occupied_seats_count(&self) -> usize {
+        self
+            .seats
+            .iter()
+            .filter(|(_coords, seat)| *seat == &'#')
+            .count()
+    }
+
     fn is_occupied(&self, seat: (i32, i32)) -> bool {
         match self.seats.get(&seat) {
-            Some(s) => *s == '#',
-            None => false,
+            Some('#') => true,
+            _ => false
         }
     }
 
+    fn adjacent_occupied_count(&self, seat: (i32, i32)) -> usize {
+        ADJACENT_TILES.iter().filter(|(x, y)| self.is_occupied((seat.0 + x, seat.1 + y))).count()
+    }
+
     fn part_1(&mut self) -> usize {
-        self.print();
+        // self.print();
+        
+        let mut is_stabilized = false;
+
+        while !is_stabilized {
+            let mut new_seats = self.seats.clone();
+            self.step += 1;
+
+            for (seat, state) in self.seats.clone() {
+                // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+                if state == 'L' && self.adjacent_occupied_count(seat) == 0 {
+                    *new_seats.entry(seat).or_insert('#') = '#';
+                }
+                // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+                else if state == '#' && self.adjacent_occupied_count(seat) >= 4 {
+                    *new_seats.entry(seat).or_insert('L') = 'L';
+                }
+            }
+
+            is_stabilized = new_seats == self.seats;
+            self.seats = new_seats;
+
+            // self.print();
+        }
+
+        self.occupied_seats_count()
+    }
+
+    fn occupied_at_direction(&self, seat: (i32, i32), direction: (i32, i32)) -> bool {
+        let current = (seat.0 + direction.0, seat.1 + direction.1);
+
+        match self.seats.get(&current) {
+            Some('#') => true,
+            Some('L') => false,
+            Some('.') => self.occupied_at_direction(current, direction),
+            None => false,
+            Some(_unknown) => false,
+        }
+    }
+
+    fn visible_occupied_count(&self, seat: (i32, i32)) -> usize {
+        ADJACENT_TILES.iter().filter(|direction| self.occupied_at_direction(seat, **direction)).count()
+    }
+
+    fn part_2(&mut self) -> usize {
+        // self.print();
 
         let mut is_stabilized = false;
 
@@ -68,22 +126,14 @@ impl GameOfSeats {
             self.step += 1;
 
             for (coords, seat) in self.seats.clone() {
-                // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-                if seat == 'L'
-                    && ADJACENT_TILES
-                        .iter()
-                        .all(|(x, y)| !self.is_occupied((coords.0 + x, coords.1 + y)))
-                {
+                // If a seat is empty (L) and there are no occupied seats in any of the
+                // visible directions, the seat becomes occupied.
+                if seat == 'L' && self.visible_occupied_count(coords) == 0 {
                     *new_seats.entry(coords).or_insert('#') = '#';
                 }
-                // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-                else if seat == '#'
-                    && ADJACENT_TILES
-                        .iter()
-                        .filter(|(x, y)| self.is_occupied((coords.0 + x, coords.1 + y)))
-                        .count()
-                        >= 4
-                {
+                // If a seat is occupied (#) and five or more seats in any of the visible
+                // directions to it are also occupied, the seat becomes empty.
+                else if seat == '#' && self.visible_occupied_count(coords) >= 5 {
                     *new_seats.entry(coords).or_insert('L') = 'L';
                 }
             }
@@ -91,21 +141,19 @@ impl GameOfSeats {
             is_stabilized = new_seats == self.seats;
             self.seats = new_seats;
 
-            self.print();
+            // self.print();
         }
 
-        return self
-            .seats
-            .iter()
-            .filter(|(_coords, seat)| *seat == &'#')
-            .count();
+        self.occupied_seats_count()
     }
 }
 
 fn main() -> () {
     let part_1_result = GameOfSeats::new(INPUT_FILE).part_1();
+    let part_2_result = GameOfSeats::new(INPUT_FILE).part_2();
 
     println!("[INFO]: Part 1: {:?}", part_1_result);
+    println!("[INFO]: Part 2: {:?}", part_2_result);
 }
 
 #[cfg(test)]
@@ -113,7 +161,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_solves_part1_example_1() {
+    fn it_solves_part1_example() {
         assert_eq!(
             GameOfSeats::new(
                 "L.LL.LL.LL\n\
@@ -130,5 +178,68 @@ mod tests {
             .part_1(),
             37
         )
+    }
+
+    #[test]
+    fn it_calculates_part2_example() {
+        assert_eq!(
+            GameOfSeats::new(
+                "L.LL.LL.LL\n\
+                 LLLLLLL.LL\n\
+                 L.L.L..L..\n\
+                 LLLL.LL.LL\n\
+                 L.LL.LL.LL\n\
+                 L.LLLLL.LL\n\
+                 ..L.L.....\n\
+                 LLLLLLLLLL\n\
+                 L.LLLLLL.L\n\
+                 L.LLLLL.LL"
+            )
+            .part_2(),
+            26
+        )
+    }
+
+    #[test]
+    fn it_finds_visible_occupied_counts() {
+        assert_eq!(
+            GameOfSeats::new(
+                ".......#.\n\
+                 ...#.....\n\
+                 .#.......\n\
+                 .........\n\
+                 ..#L....#\n\
+                 ....#....\n\
+                 .........\n\
+                 #........\n\
+                 ...#....."
+            )
+            .visible_occupied_count((3, 4)),
+            8
+        );
+
+        assert_eq!(
+            GameOfSeats::new(
+                ".............\n\
+                 .L.L.#.#.#.#.\n\
+                 ............."
+            )
+            .visible_occupied_count((1, 1)),
+            0
+        );
+
+        assert_eq!(
+            GameOfSeats::new(
+                ".##.##.\n\
+                 #.#.#.#\n\
+                 ##...##\n\
+                 ...L...\n\
+                 ##...##\n\
+                 #.#.#.#\n\
+                 .##.##."
+            )
+            .visible_occupied_count((3, 3)),
+            0
+        );
     }
 }
