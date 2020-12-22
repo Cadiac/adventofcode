b07f802d5ce618ccb90c3d711330931bf81abda2
Jaakko Husso <jaakko.t.husso@gmail.com>
2020-12-22 19:21:33 +0200

    Clean up just a little bit

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -29,6 +29,57 @@ fn find_winner(deck_1: VecDeque<usize>, deck_2: VecDeque<usize>) -> (Winner, Vec
     }
 }
 
+fn play_recursive_game(mut deck_1: VecDeque<usize>, mut deck_2: VecDeque<usize>) -> (Winner, VecDeque<usize>) {
+    let mut previous_rounds: HashSet<(VecDeque<usize>, VecDeque<usize>)> = HashSet::new();
+
+    while !deck_1.is_empty() && !deck_2.is_empty() {
+        let mut round_winner = Winner::Player1;
+
+        // Before either player deals a card, if there was a previous round in this game
+        // that had exactly the same cards in the same order in the same players' decks,
+        // the game instantly ends in a win for player 1. Previous rounds from other games are not considered.
+        // (This prevents infinite games of Recursive Combat, which everyone agrees is a bad idea.)
+        if !previous_rounds.insert((deck_1.clone(), deck_2.clone())) {
+            return (round_winner, deck_1);
+        }
+
+        // Otherwise, this round's cards must be in a new configuration;
+        // the players begin the round by each drawing the top card of their deck as normal.
+        let card_1 = deck_1.pop_front().unwrap();
+        let card_2 = deck_2.pop_front().unwrap();
+
+        // If both players have at least as many cards remaining in their deck as the value of the card
+        // they just drew, the winner of the round is determined by playing a new game of Recursive Combat
+        if deck_1.len() >= card_1 && deck_2.len() >= card_2 {
+            let mut sub_deck_1 = deck_1.clone();
+            sub_deck_1.truncate(card_1);
+            let mut sub_deck_2 = deck_2.clone();
+            sub_deck_2.truncate(card_2);
+
+            let (recursive_game_winner, _deck) = play_recursive_game(sub_deck_1, sub_deck_2);
+            round_winner = recursive_game_winner;
+        } else {
+            // Otherwise, at least one player must not have enough cards left in their deck to recurse;
+            // the winner of the round is the player with the higher-value card.
+            if card_1 > card_2 {
+                round_winner = Winner::Player1;
+            } else {
+                round_winner = Winner::Player2;
+            }
+        }
+
+        if round_winner == Winner::Player1 {
+            deck_1.push_back(card_1);
+            deck_1.push_back(card_2);
+        } else {
+            deck_2.push_back(card_2);
+            deck_2.push_back(card_1);
+        }
+    }
+
+    find_winner(deck_1, deck_2)
+}
+
 fn part_1(input: &str) -> usize {
     let mut iter = input.split("\n\n");
 
@@ -52,56 +103,6 @@ fn part_1(input: &str) -> usize {
     let (_winner, winning_deck) = find_winner(deck_1, deck_2);
     
     calculate_score(winning_deck)
-}
-
-fn play_recursive_game(mut deck_1: VecDeque<usize>, mut deck_2: VecDeque<usize>) -> (Winner, VecDeque<usize>) {
-    let mut previous_rounds: HashSet<(VecDeque<usize>, VecDeque<usize>)> = HashSet::new();
-
-    while !deck_1.is_empty() && !deck_2.is_empty() {
-        // Before either player deals a card, if there was a previous round in this game
-        // that had exactly the same cards in the same order in the same players' decks,
-        // the game instantly ends in a win for player 1. Previous rounds from other games are not considered.
-        // (This prevents infinite games of Recursive Combat, which everyone agrees is a bad idea.)
-        if !previous_rounds.insert((deck_1.clone(), deck_2.clone())) {
-            return (Winner::Player1, deck_1);
-        }
-
-        // Otherwise, this round's cards must be in a new configuration;
-        // the players begin the round by each drawing the top card of their deck as normal.
-        let card_1 = deck_1.pop_front().unwrap();
-        let card_2 = deck_2.pop_front().unwrap();
-
-        // If both players have at least as many cards remaining in their deck as the value of the card
-        // they just drew, the winner of the round is determined by playing a new game of Recursive Combat
-        if deck_1.len() as usize >= card_1 && deck_2.len() as usize >= card_2 {
-            let mut sub_deck_1 = deck_1.clone();
-            sub_deck_1.truncate(card_1);
-            let mut sub_deck_2 = deck_2.clone();
-            sub_deck_2.truncate(card_2);
-
-            let (round_winner, _winning_deck) = play_recursive_game(sub_deck_1, sub_deck_2);
-
-            if round_winner == Winner::Player1 {
-                deck_1.push_back(card_1);
-                deck_1.push_back(card_2);
-            } else {
-                deck_2.push_back(card_2);
-                deck_2.push_back(card_1);
-            }
-        } else {
-            // Otherwise, at least one player must not have enough cards left in their deck to recurse;
-            // the winner of the round is the player with the higher-value card.
-            if card_1 > card_2 {
-                deck_1.push_back(card_1);
-                deck_1.push_back(card_2);
-            } else {
-                deck_2.push_back(card_2);
-                deck_2.push_back(card_1);
-            }
-        }
-    }
-
-    find_winner(deck_1, deck_2)
 }
 
 fn part_2(input: &str) -> usize {
