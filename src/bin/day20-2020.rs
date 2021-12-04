6912b86fb19a00f7234925d58462f8bcaaaf29d8
Jaakko Husso <jaakko.husso@futurice.com>
2021-12-05 01:57:24 +0200

    Day 20 part 1 complete

--- src/bin/day20-2020.rs
+++ src/bin/day20-2020.rs
@@ -10,7 +10,7 @@ type Puzzle = HashMap<(i32, i32), Tile>;
 #[derive(Debug, Clone)]
 struct Tile {
     id: i64,
-    edges: Vec<String>,
+    edges: [String; 4]
 }
 
 impl FromStr for Tile {
@@ -49,11 +49,11 @@ impl FromStr for Tile {
             }
         }
 
-        let edges: Vec<String> = vec![
+        let edges: [String; 4] = [
             top_edge,
             right_edge.iter().collect(),
-            bottom_edge.chars().rev().collect(),
-            left_edge.iter().rev().collect(),
+            bottom_edge,
+            left_edge.iter().collect(),
         ];
 
         Ok(Tile { id, edges })
@@ -62,15 +62,23 @@ impl FromStr for Tile {
 
 impl Tile {
     fn rotate(&mut self, turns: usize) {
-        self.edges.rotate_right(turns);
+        for _turn in 0..turns {
+            self.edges = [
+                self.edges[3].chars().rev().collect(),
+                self.edges[0].clone(),
+                self.edges[1].chars().rev().collect(),
+                self.edges[2].clone(),
+            ];
+        }
     }
 
     fn flip(&mut self) {
-        self.edges = self
-            .edges
-            .iter()
-            .map(|edge| edge.chars().rev().collect())
-            .collect();
+        self.edges = [
+            self.edges[0].chars().rev().collect(),
+            self.edges[3].clone(),
+            self.edges[2].chars().rev().collect(),
+            self.edges[1].clone()
+        ];
     }
 }
 
@@ -84,28 +92,28 @@ fn parse_tiles(input: &str) -> Vec<Tile> {
 fn is_valid_tile(current_board: Puzzle, tile: Tile, cursor: (i32, i32)) -> bool {
     // above
     if let Some(neighbour) = current_board.get(&(cursor.0, cursor.1 - 1)) {
-        if tile.edges[0] != neighbour.edges[2].chars().rev().collect::<String>() {
+        if tile.edges[0] != neighbour.edges[2] {
             return false;
         }
     }
 
     // below
     if let Some(neighbour) = current_board.get(&(cursor.0, cursor.1 + 1)) {
-        if tile.edges[2] != neighbour.edges[0].chars().rev().collect::<String>() {
+        if tile.edges[2] != neighbour.edges[0] {
             return false;
         }
     }
 
     // to right
     if let Some(neighbour) = current_board.get(&(cursor.0 + 1, cursor.1)) {
-        if tile.edges[1] != neighbour.edges[3].chars().rev().collect::<String>() {
+        if tile.edges[1] != neighbour.edges[3] {
             return false;
         }
     }
 
     // to left
     if let Some(neighbour) = current_board.get(&(cursor.0 - 1, cursor.1)) {
-        if tile.edges[3] != neighbour.edges[1].chars().rev().collect::<String>() {
+        if tile.edges[3] != neighbour.edges[1] {
             return false;
         }
     }
@@ -127,18 +135,6 @@ fn build_puzzle_recursive(
     current_board: Puzzle,
     cursor: (i32, i32),
 ) -> Option<Puzzle> {
-    println!(
-        "Looking at cursor {:?}, currently found {:?} tiles, remaining {:?}",
-        cursor,
-        current_board.len(),
-        remaining_tiles.len()
-    );
-
-    if remaining_tiles.len() == 1 {
-        // for debugger
-        println!("Last remaining");
-    }
-
     // Done, we managed to place all tiles somewhere
     if remaining_tiles.is_empty() {
         return Some(current_board);
@@ -148,13 +144,11 @@ fn build_puzzle_recursive(
     let next_cursor = find_next_cursor(size, cursor);
 
     for tile in remaining_tiles.iter() {
-        // println!("Checking if tile {:?} fits to cursor {:?}", tile, cursor);
         let rest: Vec<Tile> = remaining_tiles
             .clone()
             .into_iter()
             .filter(|remaining| remaining.id != tile.id)
             .collect();
-
 
         for rotation in 0..4 {
             let mut rotated_tile = tile.clone();
@@ -162,14 +156,11 @@ fn build_puzzle_recursive(
 
             let mut next_board = current_board.clone();
             if is_valid_tile(next_board.clone(), rotated_tile.clone(), cursor) {
-                // println!("Placing tile {:?} to cursor {:?}", tile, cursor);
                 next_board.insert(cursor, rotated_tile);
 
                 if let Some(puzzle) =
                     build_puzzle_recursive(size, rest.clone(), next_board, next_cursor)
                 {
-                    println!("We succeeded, puzzle is");
-                    println!("{:?}", puzzle);
                     return Some(puzzle);
                 }
             }
@@ -178,8 +169,6 @@ fn build_puzzle_recursive(
         // Also try the other side
         let mut flipped_tile = tile.clone();
         flipped_tile.flip();
-        // println!("Checking if flipped tile {:?} fits to cursor {:?}", flipped_tile, cursor);
-
 
         for rotation in 0..4 {
             let mut rotated_tile = flipped_tile.clone();
@@ -187,14 +176,11 @@ fn build_puzzle_recursive(
 
             let mut next_board = current_board.clone();
             if is_valid_tile(next_board.clone(), rotated_tile.clone(), cursor) {
-                // println!("Placing flipped tile {:?} to cursor {:?}", tile, cursor);
                 next_board.insert(cursor, rotated_tile);
 
                 if let Some(puzzle) =
                     build_puzzle_recursive(size, rest.clone(), next_board, next_cursor)
                 {
-                    println!("We succeeded with flipped, puzzle is");
-                    println!("{:?}", puzzle);
                     return Some(puzzle);
                 }
             }
@@ -213,8 +199,11 @@ fn part_1(input: &str) -> i64 {
     println!("Solving {:?} x {:?} puzzle", size, size);
 
     if let Some(solution) = build_puzzle_recursive(size, tiles, HashMap::new(), (0, 0)) {
-        println!("Found solution to puzzle, ebin");
-        println!("{:?}", solution);
+        println!("Found solution to puzzle, {:?}", solution);
+        return solution.get(&(0, 0)).unwrap().id *
+            solution.get(&(0, size as i32 - 1)).unwrap().id *
+            solution.get(&(size as i32 - 1, size as i32 - 1)).unwrap().id *
+            solution.get(&(size as i32 - 1, 0)).unwrap().id;
     }
 
     panic!("no solution :(");
@@ -248,8 +237,8 @@ mod tests {
         assert_eq!(parsed_tile.edges.len(), 4);
         assert_eq!(parsed_tile.edges[0], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[1], "...#.##..#".to_owned());
-        assert_eq!(parsed_tile.edges[2], "###..###..".to_owned());
-        assert_eq!(parsed_tile.edges[3], ".#..#####.".to_owned());
+        assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
+        assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
     }
 
     #[test]
@@ -276,13 +265,13 @@ mod tests {
         parsed_tile.rotate(1);
         assert_eq!(parsed_tile.edges[0], ".#..#####.".to_owned());
         assert_eq!(parsed_tile.edges[1], "..##.#..#.".to_owned());
-        assert_eq!(parsed_tile.edges[2], "...#.##..#".to_owned());
-        assert_eq!(parsed_tile.edges[3], "###..###..".to_owned());
+        assert_eq!(parsed_tile.edges[2], "#..##.#...".to_owned());
+        assert_eq!(parsed_tile.edges[3], "..###..###".to_owned());
         parsed_tile.rotate(3);
         assert_eq!(parsed_tile.edges[0], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[1], "...#.##..#".to_owned());
-        assert_eq!(parsed_tile.edges[2], "###..###..".to_owned());
-        assert_eq!(parsed_tile.edges[3], ".#..#####.".to_owned());
+        assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
+        assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
     }
 
     #[test]
@@ -302,14 +291,15 @@ mod tests {
         let mut parsed_tile = tile_str.parse::<Tile>().unwrap();
         parsed_tile.flip();
         assert_eq!(parsed_tile.edges[0], ".#..#.##..".to_owned());
-        assert_eq!(parsed_tile.edges[1], "#..##.#...".to_owned());
-        assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
-        assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
+        assert_eq!(parsed_tile.edges[1], ".#####..#.".to_owned());
+        assert_eq!(parsed_tile.edges[2], "###..###..".to_owned());
+        assert_eq!(parsed_tile.edges[3], "...#.##..#".to_owned());
+
         parsed_tile.flip();
         assert_eq!(parsed_tile.edges[0], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[1], "...#.##..#".to_owned());
-        assert_eq!(parsed_tile.edges[2], "###..###..".to_owned());
-        assert_eq!(parsed_tile.edges[3], ".#..#####.".to_owned());
+        assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
+        assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
     }
 
     #[test]
