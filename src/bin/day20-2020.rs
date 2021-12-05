03775b42aa11a722d71465209d470c699d866a5e
Jaakko Husso <jaakko.husso@futurice.com>
2021-12-05 23:37:43 +0200

    Day 20 part 2, rotating data actually seems to be working!

--- src/bin/day20-2020.rs
+++ src/bin/day20-2020.rs
@@ -1,6 +1,8 @@
 const INPUT_FILE: &str = include_str!("../../inputs/day20-2020.txt");
+const EXAMPLE_FILE: &str = include_str!("../../inputs/day20-2020-example.txt");
 
 use std::collections::HashMap;
+use std::fmt::{self, Display, Formatter};
 use std::num::ParseIntError;
 use std::str::FromStr;
 
@@ -9,7 +11,8 @@ type Puzzle = HashMap<(i32, i32), Tile>;
 #[derive(Debug, Clone)]
 struct Tile {
     id: i64,
-    edges: [String; 4]
+    edges: [String; 4],
+    data: Vec<Vec<char>>,
 }
 
 impl FromStr for Tile {
@@ -32,19 +35,26 @@ impl FromStr for Tile {
         let mut bottom_edge: String = String::new();
         let mut left_edge: Vec<char> = Vec::new();
 
-        for (y, line) in lines.enumerate() {
-            if y == 0 {
-                top_edge = String::from(line);
-            } else if y == 9 {
-                bottom_edge = String::from(line);
-            }
+        let mut data: Vec<Vec<char>> = vec![];
 
+        for (y, line) in lines.enumerate() {
+            let mut chars: Vec<char> = vec![];
             for (x, pixel) in line.chars().enumerate() {
                 if x == 0 {
                     left_edge.push(pixel);
                 } else if x == 9 {
                     right_edge.push(pixel);
+                } else {
+                    chars.push(pixel);
                 }
+            }
+
+            if y == 0 {
+                top_edge = String::from(line);
+            } else if y == 9 {
+                bottom_edge = String::from(line);
+            } else {
+                data.push(chars);
             }
         }
 
@@ -55,29 +65,70 @@ impl FromStr for Tile {
             left_edge.iter().collect(),
         ];
 
-        Ok(Tile { id, edges })
+        Ok(Tile { id, edges, data })
     }
 }
 
 impl Tile {
+    // counter clockwise
     fn rotate(&mut self, turns: usize) {
         for _turn in 0..turns {
             self.edges = [
-                self.edges[3].chars().rev().collect(),
-                self.edges[0].clone(),
-                self.edges[1].chars().rev().collect(),
-                self.edges[2].clone(),
+                self.edges[1].clone(),
+                self.edges[2].chars().rev().collect(),
+                self.edges[3].clone(),
+                self.edges[0].chars().rev().collect(),
             ];
+
+            // reverse every row + transpose rotates the data counter-clockwise 90 degrees.
+            // we could probably live with that
+            // Transpose the data
+            let mut line_iters: Vec<_> = self
+                .data
+                .clone()
+                .into_iter()
+                .map(|line| line.into_iter().rev())
+                .collect();
+            self.data = (0..8)
+                .map(|_| {
+                    line_iters
+                        .iter_mut()
+                        .map(|line| line.next().unwrap())
+                        .collect::<Vec<char>>()
+                })
+                .collect();
         }
     }
 
+    // flip vertically
     fn flip(&mut self) {
         self.edges = [
             self.edges[0].chars().rev().collect(),
             self.edges[3].clone(),
             self.edges[2].chars().rev().collect(),
-            self.edges[1].clone()
+            self.edges[1].clone(),
         ];
+
+        self.data = self
+            .data
+            .iter()
+            .cloned()
+            .map(|line| line.into_iter().rev().collect())
+            .collect();
+    }
+}
+
+impl Display for Tile {
+    // `f` is a buffer, and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let output = self
+            .data
+            .iter()
+            .map(|pixel_row| pixel_row.iter().collect::<String>())
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        write!(f, "{}", output)
     }
 }
 
@@ -153,15 +204,13 @@ fn build_puzzle_recursive(
         let mut flipped_tile = tile.clone();
         flipped_tile.flip();
 
-        for tile_side in [tile, &flipped_tile]  {
+        for tile_side in [tile, &flipped_tile] {
             for rotation in 0..4 {
                 let mut rotated_tile = tile_side.clone();
                 rotated_tile.rotate(rotation);
-    
                 if is_valid_tile(&current_board, &rotated_tile, cursor) {
                     let mut next_board = current_board.clone();
                     next_board.insert(cursor, rotated_tile);
-    
                     if let Some(solution) =
                         build_puzzle_recursive(size, rest.clone(), next_board, next_cursor)
                     {
@@ -183,31 +232,70 @@ fn part_1(input: &str) -> i64 {
 
     println!("Solving {:?} x {:?} puzzle", size, size);
 
-    if let Some(solution) = build_puzzle_recursive(size, tiles, HashMap::new(), (0, 0)) {
-        println!("Found solution to puzzle, {:?}", solution);
-        return solution.get(&(0, 0)).unwrap().id *
-            solution.get(&(0, size as i32 - 1)).unwrap().id *
-            solution.get(&(size as i32 - 1, size as i32 - 1)).unwrap().id *
-            solution.get(&(size as i32 - 1, 0)).unwrap().id;
+    if let Some(puzzle) = build_puzzle_recursive(size, tiles, HashMap::new(), (0, 0)) {
+        println!("Found solution to puzzle!");
+
+        let mut full_data: Vec<Vec<char>> = vec![];
+
+        for y in 0..size as i32 {
+            let mut rows = vec![vec![]; 8];
+
+            for x in 0..size as i32 {
+                let mut tile = puzzle.get(&(x, y)).unwrap().clone();
+
+                print!(" {} ", tile.id);
+
+                rows[0].append(&mut tile.data[0]);
+                rows[0].push(' ');
+                rows[1].append(&mut tile.data[1]);
+                rows[1].push(' ');
+                rows[2].append(&mut tile.data[2]);
+                rows[2].push(' ');
+                rows[3].append(&mut tile.data[3]);
+                rows[3].push(' ');
+                rows[4].append(&mut tile.data[4]);
+                rows[4].push(' ');
+                rows[5].append(&mut tile.data[5]);
+                rows[5].push(' ');
+                rows[6].append(&mut tile.data[6]);
+                rows[6].push(' ');
+                rows[7].append(&mut tile.data[7]);
+                rows[7].push(' ');
+            }
+
+            println!("");
+
+            full_data.append(&mut rows);
+            full_data.push(vec![]);
+        }
+
+        println!("Gathered total {} rows", full_data.len());
+
+        for row in full_data {
+            println!("{}", row.iter().collect::<String>());
+        }
+
+        return puzzle.get(&(0, 0)).unwrap().id
+            * puzzle.get(&(0, size as i32 - 1)).unwrap().id
+            * puzzle.get(&(size as i32 - 1, size as i32 - 1)).unwrap().id
+            * puzzle.get(&(size as i32 - 1, 0)).unwrap().id;
     }
 
     panic!("no solution :(");
 }
 
 fn main() {
-    let part_1_result = part_1(INPUT_FILE);
+    let part_1_result = part_1(EXAMPLE_FILE);
     println!("[INFO]: Part 1: {:?}", part_1_result);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const EXAMPLE_FILE: &str = include_str!("../../inputs/day20-2020-example.txt");
 
     #[test]
     fn it_parses_tile() {
-        let tile_str = 
-            "Tile 2311:\n\
+        let tile_str = "Tile 2311:\n\
             ..##.#..#.\n\
             ##..#.....\n\
             #...##..#.\n\
@@ -236,8 +324,7 @@ mod tests {
 
     #[test]
     fn it_rotates_tile() {
-        let tile_str = 
-            "Tile 2311:\n\
+        let tile_str = "Tile 2311:\n\
             ..##.#..#.\n\
             ##..#.....\n\
             #...##..#.\n\
@@ -250,22 +337,64 @@ mod tests {
             ..###..###";
 
         let mut parsed_tile = tile_str.parse::<Tile>().unwrap();
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['#', '.', '.', '#', '.', '.', '.', '.'],
+                vec!['.', '.', '.', '#', '#', '.', '.', '#'],
+                vec!['#', '#', '#', '.', '#', '.', '.', '.'],
+                vec!['#', '.', '#', '#', '.', '#', '#', '#'],
+                vec!['#', '.', '.', '.', '#', '.', '#', '#'],
+                vec!['#', '.', '#', '.', '#', '.', '.', '#'],
+                vec!['.', '#', '.', '.', '.', '.', '#', '.'],
+                vec!['#', '#', '.', '.', '.', '#', '.', '#'],
+            ]
+        );
+
         parsed_tile.rotate(1);
         assert_eq!(parsed_tile.edges[0], ".#..#####.".to_owned());
         assert_eq!(parsed_tile.edges[1], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[2], "#..##.#...".to_owned());
         assert_eq!(parsed_tile.edges[3], "..###..###".to_owned());
+
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['.', '#', '.', '#', '#', '#', '.', '#'],
+                vec!['.', '.', '.', '#', '#', '.', '#', '.'],
+                vec!['.', '.', '.', '#', '.', '.', '.', '#'],
+                vec!['.', '#', '#', '.', '#', '#', '.', '.'],
+                vec!['#', '#', '.', '#', '.', '.', '.', '.'],
+                vec!['.', '.', '#', '#', '.', '#', '.', '.'],
+                vec!['.', '.', '#', '.', '.', '.', '#', '#'],
+                vec!['#', '.', '#', '#', '#', '#', '.', '#']
+            ]
+        );
+
         parsed_tile.rotate(3);
         assert_eq!(parsed_tile.edges[0], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[1], "...#.##..#".to_owned());
         assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
         assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
+
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['#', '.', '.', '#', '.', '.', '.', '.'],
+                vec!['.', '.', '.', '#', '#', '.', '.', '#'],
+                vec!['#', '#', '#', '.', '#', '.', '.', '.'],
+                vec!['#', '.', '#', '#', '.', '#', '#', '#'],
+                vec!['#', '.', '.', '.', '#', '.', '#', '#'],
+                vec!['#', '.', '#', '.', '#', '.', '.', '#'],
+                vec!['.', '#', '.', '.', '.', '.', '#', '.'],
+                vec!['#', '#', '.', '.', '.', '#', '.', '#'],
+            ]
+        );
     }
 
     #[test]
     fn it_flips_tile() {
-        let tile_str =
-            "Tile 2311:\n\
+        let tile_str = "Tile 2311:\n\
             ..##.#..#.\n\
             ##..#.....\n\
             #...##..#.\n\
@@ -278,17 +407,60 @@ mod tests {
             ..###..###";
 
         let mut parsed_tile = tile_str.parse::<Tile>().unwrap();
+
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['#', '.', '.', '#', '.', '.', '.', '.'],
+                vec!['.', '.', '.', '#', '#', '.', '.', '#'],
+                vec!['#', '#', '#', '.', '#', '.', '.', '.'],
+                vec!['#', '.', '#', '#', '.', '#', '#', '#'],
+                vec!['#', '.', '.', '.', '#', '.', '#', '#'],
+                vec!['#', '.', '#', '.', '#', '.', '.', '#'],
+                vec!['.', '#', '.', '.', '.', '.', '#', '.'],
+                vec!['#', '#', '.', '.', '.', '#', '.', '#'],
+            ]
+        );
+
         parsed_tile.flip();
         assert_eq!(parsed_tile.edges[0], ".#..#.##..".to_owned());
         assert_eq!(parsed_tile.edges[1], ".#####..#.".to_owned());
         assert_eq!(parsed_tile.edges[2], "###..###..".to_owned());
         assert_eq!(parsed_tile.edges[3], "...#.##..#".to_owned());
 
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['.', '.', '.', '.', '#', '.', '.', '#'],
+                vec!['#', '.', '.', '#', '#', '.', '.', '.'],
+                vec!['.', '.', '.', '#', '.', '#', '#', '#'],
+                vec!['#', '#', '#', '.', '#', '#', '.', '#'],
+                vec!['#', '#', '.', '#', '.', '.', '.', '#'],
+                vec!['#', '.', '.', '#', '.', '#', '.', '#'],
+                vec!['.', '#', '.', '.', '.', '.', '#', '.'],
+                vec!['#', '.', '#', '.', '.', '.', '#', '#']
+            ]
+        );
+
         parsed_tile.flip();
         assert_eq!(parsed_tile.edges[0], "..##.#..#.".to_owned());
         assert_eq!(parsed_tile.edges[1], "...#.##..#".to_owned());
         assert_eq!(parsed_tile.edges[2], "..###..###".to_owned());
         assert_eq!(parsed_tile.edges[3], ".#####..#.".to_owned());
+
+        assert_eq!(
+            parsed_tile.data,
+            vec![
+                vec!['#', '.', '.', '#', '.', '.', '.', '.'],
+                vec!['.', '.', '.', '#', '#', '.', '.', '#'],
+                vec!['#', '#', '#', '.', '#', '.', '.', '.'],
+                vec!['#', '.', '#', '#', '.', '#', '#', '#'],
+                vec!['#', '.', '.', '.', '#', '.', '#', '#'],
+                vec!['#', '.', '#', '.', '#', '.', '.', '#'],
+                vec!['.', '#', '.', '.', '.', '.', '#', '.'],
+                vec!['#', '#', '.', '.', '.', '#', '.', '#'],
+            ]
+        );
     }
 
     #[test]
