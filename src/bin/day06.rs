363f2f2188e4c2bc2f2a6ecf83a117fa9ce0bad0
Jaakko Husso <jaakko.husso@futurice.com>
2021-12-06 23:51:38 +0200

    Day 06 minor refactoring after reading other solutions, why did I not just start with an empty generation each day?

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -12,37 +12,35 @@ fn parse(input: &str) -> Vec<usize> {
 // Similarly, at any given day each fish with equal current timers will end up
 // producing the same amount of total fish, so only the counts of fish with
 // each different timer need to be considered.
-fn solve(input: &str, days: usize) -> i64 {
-    let mut fish_by_timer: [i64; 9] = [0; 9];
+fn solve(input: &str, days: usize) -> u64 {
+    let mut fish_generations: [u64; 9] = [0; 9];
 
     for initial_timer in parse(input).into_iter() {
-        fish_by_timer[initial_timer] += 1;
+        fish_generations[initial_timer] += 1;
     }
 
     for _day in 0..days {
-        let mut next_fish_by_timer = fish_by_timer;
+        let mut next_fish_generations = [0; 9];
 
-        for timer in 0..fish_by_timer.len() {
-            let count = fish_by_timer[timer];
+        for timer in 0..fish_generations.len() {
+            let count = fish_generations[timer];
 
             if timer == 0 {
                 // The fish are ready to produce offspring -
                 // after this day each fish with this internal timer resets to 6,
                 // and they each create a new lanternfish with an internal timer of 8.
-                next_fish_by_timer[6] += count;
-                next_fish_by_timer[8] += count;
+                next_fish_generations[6] += count;
+                next_fish_generations[8] += count;
             } else {
                 // Normally their internal timer just decreases by one
-                next_fish_by_timer[timer - 1] += count;
+                next_fish_generations[timer - 1] += count;
             }
-
-            next_fish_by_timer[timer] -= count;
         }
 
-        fish_by_timer = next_fish_by_timer;
+        fish_generations = next_fish_generations;
     }
 
-    fish_by_timer.iter().sum()
+    fish_generations.iter().sum()
 }
 
 fn main() {
