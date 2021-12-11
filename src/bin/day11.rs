efca81fcb295c79be23c8501f9884fffbcbe177e
Jaakko Husso <jaakko.husso@futurice.com>
2021-12-11 18:08:38 +0200

    Revert "Day 11 - refactor using hashmaps for storing the octopuses for cleaner neighbour handling, but this required some cloning so maybe this wasn't such a good idea after all."
    This reverts commit 9d6e79f6936777dd5cd22ed50e8b9dda04fb9656.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,50 +1,69 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 const INPUT_FILE: &str = include_str!("../../inputs/day11.txt");
 
-fn parse(input: &str) -> HashMap<(i32, i32), u32> {
-    let mut octopuses = HashMap::new();
-    for (y, line) in input.lines().enumerate() {
-        for (x, energy_level) in line.chars().enumerate() {
-            if let Some(energy_level) = energy_level.to_digit(10) {
-                octopuses.insert((x as i32, y as i32), energy_level);
-            }
-        }
-    }
-    octopuses
+fn parse(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|energy_level| energy_level.to_digit(10).unwrap())
+                .collect()
+        })
+        .collect()
 }
 
-fn simulate_step(mut octopuses: HashMap<(i32, i32), u32>) -> (HashMap<(i32, i32), u32>, usize) {
-    let mut flashed: HashSet<(i32, i32)> = HashSet::new();
+fn simulate_step(mut octopuses: Vec<Vec<u32>>) -> (Vec<Vec<u32>>, usize) {
+    let mut flashed: HashSet<(usize, usize)> = HashSet::new();
 
-    for energy_level in octopuses.values_mut() {
-        *energy_level += 1;
+    for row in &mut octopuses {
+        for octopus in row {
+            // First, the energy level of each octopus increases by 1.
+            *octopus += 1;
+        }
     }
 
     loop {
         let mut some_octopus_flashed = false;
 
-        for ((x, y), energy_level) in octopuses.clone() {
-            // Then, any octopus with an energy level greater than 9 flashes.
-            if energy_level > 9 {
-                // An octopus can only flash at most once per step.
-                if flashed.insert((x, y)) {
-                    some_octopus_flashed = true;
+        for y in 0..octopuses.len() {
+            for x in 0..octopuses[y].len() {
+                // Then, any octopus with an energy level greater than 9 flashes.
+                if octopuses[y][x] > 9 {
+                    // An octopus can only flash at most once per step.
+                    if flashed.insert((x, y)) {
+                        some_octopus_flashed = true;
 
-                    // This increases the energy level of all adjacent octopuses by 1,
-                    // including octopuses that are diagonally adjacent.
-                    // If this causes an octopus to have an energy level greater than 9, it also flashes.
-                    #[rustfmt::skip]
-                    let offsets = [
-                        (-1, 1), (0, 1), (1, 1),
-                        (-1, 0),         (1, 0),
-                        (-1,-1), (0,-1), (1,-1),
-                    ];
+                        // This increases the energy level of all adjacent octopuses by 1,
+                        // including octopuses that are diagonally adjacent.
+                        // If this causes an octopus to have an energy level greater than 9, it also flashes.
 
-                    for (offset_x, offset_y) in offsets {
-                        if let Some(neighbour) = octopuses.get_mut(&(x + offset_x, y + offset_y)) {
-                            *neighbour += 1;
+                        if y > 0 {
+                            octopuses[y - 1][x] += 1;
+                            if x > 0 {
+                                octopuses[y - 1][x - 1] += 1;
+                            }
+                            if x + 1 < octopuses[y - 1].len() {
+                                octopuses[y - 1][x + 1] += 1;
+                            }
+                        }
+
+                        if y + 1 < octopuses.len() {
+                            octopuses[y + 1][x] += 1;
+                            if x > 0 {
+                                octopuses[y + 1][x - 1] += 1;
+                            }
+                            if x + 1 < octopuses[y + 1].len() {
+                                octopuses[y + 1][x + 1] += 1;
+                            }
+                        }
+
+                        if x > 0 {
+                            octopuses[y][x - 1] += 1;
+                        }
+
+                        if x + 1 < octopuses[y].len() {
+                            octopuses[y][x + 1] += 1;
                         }
                     }
                 }
@@ -59,13 +78,13 @@ fn simulate_step(mut octopuses: HashMap<(i32, i32), u32>) -> (HashMap<(i32, i32)
 
     // Finally, any octopus that flashed during this step has its energy level set to 0, as
     // it used all of its energy to flash.
-    for coords in flashed.iter() {
-        if let Some(octopus) = octopuses.get_mut(coords) {
-            *octopus = 0;
-        }
+    let count = flashed.len();
+
+    for (x, y) in flashed.into_iter() {
+        octopuses[y][x] = 0;
     }
 
-    (octopuses, flashed.len())
+    (octopuses, count)
 }
 
 fn part_1(input: &str, steps: usize) -> usize {
@@ -84,11 +103,13 @@ fn part_1(input: &str, steps: usize) -> usize {
 fn part_2(input: &str) -> usize {
     let mut octopuses = parse(input);
 
+    let total_octopuses_count = octopuses.iter().map(|row| row.len()).sum();
+
     let mut steps = 1;
 
     loop {
         let (new_octopuses, flashes) = simulate_step(std::mem::take(&mut octopuses));
-        if flashes == new_octopuses.len() {
+        if flashes == total_octopuses_count {
             return steps;
         }
 
