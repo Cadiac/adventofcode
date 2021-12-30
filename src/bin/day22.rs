1b3bd98a053dbb768fbe17142c7dddd323038417
Jaakko Husso <jaakko.husso@futurice.com>
2021-12-30 23:03:57 +0200

    Day 22 - Some cleanup, do both parts with the part 2 solution.

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,61 +1,20 @@
 extern crate serde_scan;
 use std::cmp::{max, min};
-use std::collections::HashSet;
-
-type Region = ((i64, i64), (i64, i64), (i64, i64));
 
 const INPUT_FILE: &str = include_str!("../../inputs/day22.txt");
 
-fn part_1(input: &str, bounds: Region) -> usize {
-    let max_size =
-        (bounds.0 .1 - bounds.0 .0) * (bounds.1 .1 - bounds.1 .0) * (bounds.2 .1 - bounds.2 .0);
-    let mut cubes: HashSet<(i64, i64, i64)> = HashSet::with_capacity(max_size as usize);
-
-    input.lines().for_each(|line| {
-        let (state, cuboid): (&str, Region) =
-            serde_scan::scan!("{} x={}..{},y={}..{},z={}..{}" <- line).unwrap();
-
-        let x_min = max(cuboid.0 .0, bounds.0 .0);
-        let x_max = min(cuboid.0 .1, bounds.0 .1);
-        let y_min = max(cuboid.1 .0, bounds.1 .0);
-        let y_max = min(cuboid.1 .1, bounds.1 .1);
-        let z_min = max(cuboid.2 .0, bounds.2 .0);
-        let z_max = min(cuboid.2 .1, bounds.2 .1);
-
-        if state == "on" {
-            for x in x_min..=x_max {
-                for y in y_min..=y_max {
-                    for z in z_min..=z_max {
-                        cubes.insert((x, y, z));
-                    }
-                }
-            }
-        } else if state == "off" {
-            for x in x_min..=x_max {
-                for y in y_min..=y_max {
-                    for z in z_min..=z_max {
-                        cubes.remove(&(x, y, z));
-                    }
-                }
-            }
-        }
-    });
-
-    cubes.len()
-}
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Cuboid {
-    x_min: i64,
-    x_max: i64,
-    y_min: i64,
-    y_max: i64,
-    z_min: i64,
-    z_max: i64,
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
+    z_min: i32,
+    z_max: i32,
 }
 
 impl Cuboid {
-    fn new(x_min: i64, x_max: i64, y_min: i64, y_max: i64, z_min: i64, z_max: i64) -> Self {
+    fn new(x_min: i32, x_max: i32, y_min: i32, y_max: i32, z_min: i32, z_max: i32) -> Self {
         Self {
             x_min,
             x_max,
@@ -166,14 +125,29 @@ impl Cuboid {
     }
 }
 
-fn part_2(input: &str) -> usize {
+fn reboot_reactor(input: &str, initialization_procedure: bool) -> usize {
     let mut cuboids: Vec<Cuboid> = Vec::new();
 
     for line in input.lines() {
-        let (state, ((x_min, x_max), (y_min, y_max), (z_min, z_max))): (&str, Region) =
+        let (state, (x_min, x_max, y_min, y_max, z_min, z_max)): (&str, _) =
             serde_scan::scan!("{} x={}..{},y={}..{},z={}..{}" <- line).unwrap();
 
-        let cuboid = Cuboid::new(x_min, x_max + 1, y_min, y_max + 1, z_min, z_max + 1);
+        let cuboid = if initialization_procedure {
+            if x_min > 50 || x_max < -50 || y_min > 50 || y_max < -50 || z_min > 50 || z_max < -50 {
+                continue;
+            }
+
+            Cuboid::new(
+                max(x_min, -50),
+                min(x_max, 50) + 1,
+                max(y_min, -50),
+                min(y_max, 50) + 1,
+                max(z_min, -50),
+                min(z_max, 50) + 1,
+            )
+        } else {
+            Cuboid::new(x_min, x_max + 1, y_min, y_max + 1, z_min, z_max + 1)
+        };
 
         // First remove the intersection of this cuboid from any discovered cuboids
         cuboids = cuboids
@@ -191,10 +165,10 @@ fn part_2(input: &str) -> usize {
 }
 
 fn main() {
-    let part_1_result = part_1(INPUT_FILE, ((-50, 50), (-50, 50), (-50, 50)));
+    let part_1_result = reboot_reactor(INPUT_FILE, true);
     println!("[INFO]: Part 1: {:?}", part_1_result);
 
-    let part_2_result = part_2(INPUT_FILE);
+    let part_2_result = reboot_reactor(INPUT_FILE, false);
     println!("[INFO]: Part 2: {:?}", part_2_result);
 }
 
@@ -205,25 +179,12 @@ mod tests {
     #[test]
     fn it_solves_part1_small_example() {
         assert_eq!(
-            part_1(
+            reboot_reactor(
                 "on x=10..12,y=10..12,z=10..12\n\
                  on x=11..13,y=11..13,z=11..13\n\
                  off x=9..11,y=9..11,z=9..11\n\
                  on x=10..10,y=10..10,z=10..10",
-                ((-50, 50), (-50, 50), (-50, 50))
-            ),
-            39
-        );
-    }
-
-    #[test]
-    fn it_solves_part1_small_example_with_part2() {
-        assert_eq!(
-            part_2(
-                "on x=10..12,y=10..12,z=10..12\n\
-                 on x=11..13,y=11..13,z=11..13\n\
-                 off x=9..11,y=9..11,z=9..11\n\
-                 on x=10..10,y=10..10,z=10..10",
+                true
             ),
             39
         );
@@ -232,7 +193,7 @@ mod tests {
     #[test]
     fn it_solves_part1_large_example() {
         assert_eq!(
-            part_1(
+            reboot_reactor(
                 "on x=-20..26,y=-36..17,z=-47..7\n\
                  on x=-20..33,y=-21..23,z=-26..28\n\
                  on x=-22..28,y=-29..23,z=-38..16\n\
@@ -255,36 +216,7 @@ mod tests {
                  on x=-41..9,y=-7..43,z=-33..15\n\
                  on x=-54112..-39298,y=-85059..-49293,z=-27449..7877\n\
                  on x=967..23432,y=45373..81175,z=27513..53682",
-                ((-50, 50), (-50, 50), (-50, 50))
-            ),
-            590784
-        );
-    }
-
-    #[test]
-    fn it_solves_part1_large_example_with_part2() {
-        assert_eq!(
-            part_2(
-                "on x=-20..26,y=-36..17,z=-47..7\n\
-                 on x=-20..33,y=-21..23,z=-26..28\n\
-                 on x=-22..28,y=-29..23,z=-38..16\n\
-                 on x=-46..7,y=-6..46,z=-50..-1\n\
-                 on x=-49..1,y=-3..46,z=-24..28\n\
-                 on x=2..47,y=-22..22,z=-23..27\n\
-                 on x=-27..23,y=-28..26,z=-21..29\n\
-                 on x=-39..5,y=-6..47,z=-3..44\n\
-                 on x=-30..21,y=-8..43,z=-13..34\n\
-                 on x=-22..26,y=-27..20,z=-29..19\n\
-                 off x=-48..-32,y=26..41,z=-47..-37\n\
-                 on x=-12..35,y=6..50,z=-50..-2\n\
-                 off x=-48..-32,y=-32..-16,z=-15..-5\n\
-                 on x=-18..26,y=-33..15,z=-7..46\n\
-                 off x=-40..-22,y=-38..-28,z=23..41\n\
-                 on x=-16..35,y=-41..10,z=-47..6\n\
-                 off x=-32..-23,y=11..30,z=-14..3\n\
-                 on x=-49..-5,y=-3..45,z=-29..18\n\
-                 off x=18..30,y=-20..-8,z=-3..13\n\
-                 on x=-41..9,y=-7..43,z=-33..15"
+                true
             ),
             590784
         );
@@ -293,26 +225,7 @@ mod tests {
     #[test]
     fn it_solves_part2_example_1() {
         assert_eq!(
-            part_2(
-                "on x=-5..47,y=-31..22,z=-19..33\n\
-                 on x=-44..5,y=-27..21,z=-14..35\n\
-                 on x=-49..-1,y=-11..42,z=-10..38\n\
-                 on x=-20..34,y=-40..6,z=-44..1\n\
-                 off x=26..39,y=40..50,z=-2..11\n\
-                 on x=-41..5,y=-41..6,z=-36..8\n\
-                 off x=-43..-33,y=-45..-28,z=7..25\n\
-                 on x=-33..15,y=-32..19,z=-34..11\n\
-                 off x=35..47,y=-46..-34,z=-11..5\n\
-                 on x=-14..36,y=-6..44,z=-16..29"
-            ),
-            474140
-        );
-    }
-
-    #[test]
-    fn it_solves_part2_example_2() {
-        assert_eq!(
-            part_2(
+            reboot_reactor(
                 "on x=-5..47,y=-31..22,z=-19..33\n\
                  on x=-44..5,y=-27..21,z=-14..35\n\
                  on x=-49..-1,y=-11..42,z=-10..38\n\
@@ -372,7 +285,78 @@ mod tests {
                  off x=-27365..46395,y=31009..98017,z=15428..76570\n\
                  off x=-70369..-16548,y=22648..78696,z=-1892..86821\n\
                  on x=-53470..21291,y=-120233..-33476,z=-44150..38147\n\
-                 off x=-93533..-4276,y=-16170..68771,z=-104985..-24507"
+                 off x=-93533..-4276,y=-16170..68771,z=-104985..-24507",
+                true
+            ),
+            474140
+        );
+    }
+
+    #[test]
+    fn it_solves_part2_example_2() {
+        assert_eq!(
+            reboot_reactor(
+                "on x=-5..47,y=-31..22,z=-19..33\n\
+                 on x=-44..5,y=-27..21,z=-14..35\n\
+                 on x=-49..-1,y=-11..42,z=-10..38\n\
+                 on x=-20..34,y=-40..6,z=-44..1\n\
+                 off x=26..39,y=40..50,z=-2..11\n\
+                 on x=-41..5,y=-41..6,z=-36..8\n\
+                 off x=-43..-33,y=-45..-28,z=7..25\n\
+                 on x=-33..15,y=-32..19,z=-34..11\n\
+                 off x=35..47,y=-46..-34,z=-11..5\n\
+                 on x=-14..36,y=-6..44,z=-16..29\n\
+                 on x=-57795..-6158,y=29564..72030,z=20435..90618\n\
+                 on x=36731..105352,y=-21140..28532,z=16094..90401\n\
+                 on x=30999..107136,y=-53464..15513,z=8553..71215\n\
+                 on x=13528..83982,y=-99403..-27377,z=-24141..23996\n\
+                 on x=-72682..-12347,y=18159..111354,z=7391..80950\n\
+                 on x=-1060..80757,y=-65301..-20884,z=-103788..-16709\n\
+                 on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856\n\
+                 on x=-52752..22273,y=-49450..9096,z=54442..119054\n\
+                 on x=-29982..40483,y=-108474..-28371,z=-24328..38471\n\
+                 on x=-4958..62750,y=40422..118853,z=-7672..65583\n\
+                 on x=55694..108686,y=-43367..46958,z=-26781..48729\n\
+                 on x=-98497..-18186,y=-63569..3412,z=1232..88485\n\
+                 on x=-726..56291,y=-62629..13224,z=18033..85226\n\
+                 on x=-110886..-34664,y=-81338..-8658,z=8914..63723\n\
+                 on x=-55829..24974,y=-16897..54165,z=-121762..-28058\n\
+                 on x=-65152..-11147,y=22489..91432,z=-58782..1780\n\
+                 on x=-120100..-32970,y=-46592..27473,z=-11695..61039\n\
+                 on x=-18631..37533,y=-124565..-50804,z=-35667..28308\n\
+                 on x=-57817..18248,y=49321..117703,z=5745..55881\n\
+                 on x=14781..98692,y=-1341..70827,z=15753..70151\n\
+                 on x=-34419..55919,y=-19626..40991,z=39015..114138\n\
+                 on x=-60785..11593,y=-56135..2999,z=-95368..-26915\n\
+                 on x=-32178..58085,y=17647..101866,z=-91405..-8878\n\
+                 on x=-53655..12091,y=50097..105568,z=-75335..-4862\n\
+                 on x=-111166..-40997,y=-71714..2688,z=5609..50954\n\
+                 on x=-16602..70118,y=-98693..-44401,z=5197..76897\n\
+                 on x=16383..101554,y=4615..83635,z=-44907..18747\n\
+                 off x=-95822..-15171,y=-19987..48940,z=10804..104439\n\
+                 on x=-89813..-14614,y=16069..88491,z=-3297..45228\n\
+                 on x=41075..99376,y=-20427..49978,z=-52012..13762\n\
+                 on x=-21330..50085,y=-17944..62733,z=-112280..-30197\n\
+                 on x=-16478..35915,y=36008..118594,z=-7885..47086\n\
+                 off x=-98156..-27851,y=-49952..43171,z=-99005..-8456\n\
+                 off x=2032..69770,y=-71013..4824,z=7471..94418\n\
+                 on x=43670..120875,y=-42068..12382,z=-24787..38892\n\
+                 off x=37514..111226,y=-45862..25743,z=-16714..54663\n\
+                 off x=25699..97951,y=-30668..59918,z=-15349..69697\n\
+                 off x=-44271..17935,y=-9516..60759,z=49131..112598\n\
+                 on x=-61695..-5813,y=40978..94975,z=8655..80240\n\
+                 off x=-101086..-9439,y=-7088..67543,z=33935..83858\n\
+                 off x=18020..114017,y=-48931..32606,z=21474..89843\n\
+                 off x=-77139..10506,y=-89994..-18797,z=-80..59318\n\
+                 off x=8476..79288,y=-75520..11602,z=-96624..-24783\n\
+                 on x=-47488..-1262,y=24338..100707,z=16292..72967\n\
+                 off x=-84341..13987,y=2429..92914,z=-90671..-1318\n\
+                 off x=-37810..49457,y=-71013..-7894,z=-105357..-13188\n\
+                 off x=-27365..46395,y=31009..98017,z=15428..76570\n\
+                 off x=-70369..-16548,y=22648..78696,z=-1892..86821\n\
+                 on x=-53470..21291,y=-120233..-33476,z=-44150..38147\n\
+                 off x=-93533..-4276,y=-16170..68771,z=-104985..-24507",
+                false
             ),
             2758514936282235
         );
