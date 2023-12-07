9b11b79ce6d4b202162dc8268db1f3cae0a0440b
Jaakko Husso <jaakko.t.husso@gmail.com>
2023-12-08 01:03:33 +0200

    Day 07 solutions

--- aoc-solver/src/y2023/day07.rs
+++ aoc-solver/src/y2023/day07.rs
@@ -1,26 +1,58 @@
-use std::{collections::HashMap, cmp::Ordering};
+use std::collections::HashMap;
 
 use itertools::Itertools;
-use log::info;
 
 use crate::solution::{AocError, Solution};
 
+const SCORES_PART_1: [char; 13] = [
+    'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
+];
+const SCORES_PART_2: [char; 13] = [
+    'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+];
+
 pub struct Day07;
 
-#[derive(Debug, Clone)]
-struct Card {}
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0,
+}
 
-type Hand = [u8; 5];
+type Cards = [u8; 5];
+
+#[derive(PartialEq, Eq, Debug)]
+struct Hand {
+    cards: Cards,
+    kind: HandType,
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(std::cmp::Ord::cmp(self, other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.kind
+            .cmp(&other.kind)
+            .then(self.cards.cmp(&other.cards))
+    }
+}
 
 fn parse(input: &str) -> Result<Vec<(Hand, u32)>, AocError> {
-    let card_scores: HashMap<char, u8> = [
-        'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
-    ]
-    .into_iter()
-    .rev()
-    .enumerate()
-    .map(|(score, card)| (card, score as u8))
-    .collect();
+    let card_scores: HashMap<char, u8> = SCORES_PART_1
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(score, card)| (card, score as u8))
+        .collect();
 
     let hands = input
         .trim()
@@ -30,8 +62,10 @@ fn parse(input: &str) -> Result<Vec<(Hand, u32)>, AocError> {
                 .split_once(' ')
                 .ok_or(AocError::parse(line, "Invalid line"))?;
 
-            let hand = parse_cards(hand_str, &card_scores)?;
+            let cards = parse_cards(hand_str, &card_scores)?;
             let bid = parse_number(bid_str)?;
+            let kind = hand_type(cards);
+            let hand = Hand { cards, kind };
 
             Ok((hand, bid))
         })
@@ -40,7 +74,35 @@ fn parse(input: &str) -> Result<Vec<(Hand, u32)>, AocError> {
     Ok(hands)
 }
 
-fn parse_cards(cards: &str, card_scores: &HashMap<char, u8>) -> Result<Hand, AocError> {
+fn parse2(input: &str) -> Result<Vec<(Hand, u32)>, AocError> {
+    let card_scores: HashMap<char, u8> = SCORES_PART_2
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(score, card)| (card, score as u8))
+        .collect();
+
+    let hands = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let (hand_str, bid_str) = line
+                .split_once(' ')
+                .ok_or(AocError::parse(line, "Invalid line"))?;
+
+            let cards = parse_cards(hand_str, &card_scores)?;
+            let bid = parse_number(bid_str)?;
+            let kind = hand_type_jokers(cards);
+            let hand = Hand { cards, kind };
+
+            Ok((hand, bid))
+        })
+        .collect::<Result<_, _>>()?;
+
+    Ok(hands)
+}
+
+fn parse_cards(cards: &str, card_scores: &HashMap<char, u8>) -> Result<Cards, AocError> {
     cards
         .chars()
         .take(5)
@@ -61,54 +123,71 @@ fn parse_number(number: &str) -> Result<u32, AocError> {
         .map_err(|_| AocError::parse(number, "Error parsing number"))
 }
 
-fn score_hand(hand: Hand) -> u32 {
+fn hand_type(cards: Cards) -> HandType {
     let mut counts_by_card: HashMap<u8, u8> = HashMap::new();
 
-    for card in hand {
+    for card in cards {
         *counts_by_card.entry(card).or_default() += 1;
     }
 
     if counts_by_card.values().any(|count| *count == 5) {
-        return 0;
+        return HandType::FiveOfAKind;
     }
 
     if counts_by_card.values().any(|count| *count == 4) {
-        return 1;
+        return HandType::FourOfAKind;
     }
 
-    if counts_by_card.values().any(|count| *count == 3) && counts_by_card.values().any(|count| *count == 2) {
-        return 2;
+    if counts_by_card.values().any(|count| *count == 3)
+        && counts_by_card.values().any(|count| *count == 2)
+    {
+        return HandType::FullHouse;
     }
 
     if counts_by_card.values().any(|count| *count == 3) {
-        return 4;
+        return HandType::ThreeOfAKind;
     }
 
-    if counts_by_card.values().any(|count| *count == 2) && counts_by_card.values().any(|count| *count == 2) {
-        return 3;
-    }
-
-    if counts_by_card.values().any(|count| *count == 2) {
-        return 5;
-    }
-
-    return 6;
-}
-
-fn sort_hands(a: &(Hand, u32, u32), b: &(Hand, u32, u32)) -> Ordering {
-    match a.1.partial_cmp(&b.1) {
-        Some(Ordering::Equal) | None => (),
-        Some(ordering) => return ordering
-    }
-
-    for i in 0..5 {
-        match a.0[i].partial_cmp(&b.0[i]) {
-            Some(Ordering::Equal) | None => (),
-            Some(ordering) => return ordering
+    let mut two_pairs = 0;
+    for count in counts_by_card.values() {
+        if *count == 2 {
+            two_pairs += 1
         }
     }
 
-    Ordering::Equal
+    if two_pairs == 2 {
+        return HandType::TwoPair;
+    }
+
+    if counts_by_card.values().any(|count| *count == 2) {
+        return HandType::OnePair;
+    }
+
+    HandType::HighCard
+}
+
+fn hand_type_jokers(cards: Cards) -> HandType {
+    let mut counts_by_card: HashMap<u8, u8> = HashMap::new();
+
+    for card in cards {
+        *counts_by_card.entry(card).or_default() += 1;
+    }
+
+    let jokers = counts_by_card.remove(&0).unwrap_or(0);
+    let mut others = counts_by_card.values().sorted().rev();
+
+    let first = others.next().unwrap_or(&0) + jokers;
+    let second = others.next().unwrap_or(&0);
+
+    match (first, second) {
+        (5, _) => HandType::FiveOfAKind,
+        (4, _) => HandType::FourOfAKind,
+        (3, 2) => HandType::FullHouse,
+        (3, _) => HandType::ThreeOfAKind,
+        (2, 2) => HandType::TwoPair,
+        (2, _) => HandType::OnePair,
+        _ => HandType::HighCard,
+    }
 }
 
 impl Solution for Day07 {
@@ -122,24 +201,23 @@ impl Solution for Day07 {
     fn part_1(&self, input: &str) -> Result<u32, AocError> {
         let total_winnings = parse(input)?
             .into_iter()
-            .map(|(hand, bid)| (hand, score_hand(hand), bid))
-            .sorted_by(sort_hands)
-            .collect::<Vec<_>>();
-
-        let _ = total_winnings.iter().enumerate().map(|(rank, (hand, score, bid))| {
-            info!("{rank}: {hand:?} {score} {bid}")
-        });
-
-        let total_winnings = total_winnings.iter()
+            .sorted()
             .enumerate()
-            .map(|(rank, (_, _, bid))| (rank as u32 + 1) * bid)
+            .map(|(rank, (_, bid))| (rank as u32 + 1) * bid)
             .sum();
 
         Ok(total_winnings)
     }
 
     fn part_2(&self, input: &str) -> Result<u32, AocError> {
-        unimplemented!();
+        let total_winnings = parse2(input)?
+            .into_iter()
+            .sorted()
+            .enumerate()
+            .map(|(rank, (_, bid))| (rank as u32 + 1) * bid)
+            .sum();
+
+        Ok(total_winnings)
     }
 }
 
@@ -162,10 +240,27 @@ mod tests {
     }
 
     #[test]
-    fn it_solves_part1_real() {
+    fn it_solves_part2_example() {
         assert_eq!(
-            Day07.part_1(Day07.default_input()),
-            Ok(6440)
+            Day07.part_2(
+                "32T3K 765\n\
+                 T55J5 684\n\
+                 KK677 28\n\
+                 KTJJT 220\n\
+                 QQQJA 483\n"
+            ),
+            Ok(5905)
         );
+    }
+
+    #[test]
+    fn it_finds_correct_hand_types() {
+        assert_eq!(hand_type_jokers([11, 5, 2, 12, 10]), HandType::HighCard);
+        assert_eq!(hand_type_jokers([0, 6, 12, 8, 4]), HandType::OnePair);
+        assert_eq!(hand_type_jokers([4, 1, 4, 10, 1]), HandType::TwoPair);
+        assert_eq!(hand_type_jokers([1, 5, 5, 0, 3]), HandType::ThreeOfAKind);
+        assert_eq!(hand_type_jokers([10, 10, 7, 7, 0]), HandType::FullHouse);
+        assert_eq!(hand_type_jokers([12, 4, 4, 0, 4]), HandType::FourOfAKind);
+        assert_eq!(hand_type_jokers([12, 12, 0, 12, 0]), HandType::FiveOfAKind);
     }
 }
