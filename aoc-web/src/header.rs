56636c1e4acf55e1224fa09a5acb0284139de1aa
Jaakko Husso <jaakko.t.husso@gmail.com>
2023-12-14 11:59:30 +0200

    Show the days 13 and 14 on web despite 13 not being finished

--- aoc-web/src/header.rs
+++ aoc-web/src/header.rs
@@ -31,6 +31,8 @@ pub fn header(props: &HeaderProps) -> Html {
                                     <NavLink route={Route::Solution { year: 2023, day: 10 }} current={props.route.clone()} text={"10"}/>
                                     <NavLink route={Route::Solution { year: 2023, day: 11 }} current={props.route.clone()} text={"11"}/>
                                     <NavLink route={Route::Solution { year: 2023, day: 12 }} current={props.route.clone()} text={"12"}/>
+                                    <NavLink route={Route::Solution { year: 2023, day: 13 }} current={props.route.clone()} text={"13"}/>
+                                    <NavLink route={Route::Solution { year: 2023, day: 14 }} current={props.route.clone()} text={"14"}/>
                                 </>
                             }
                         },
