5d0097217a7cc7571935a34951b8239cdb575ac3
Jaakko Husso <jaakko.t.husso@gmail.com>
2024-12-04 22:43:13 +0200

    Day 04 solutions

---
+++ aoc-solver/src/y2024/day04.rs
@@ -0,0 +1,175 @@
+use crate::solution::{AocError, Solution};
+
+type Grid = Vec<Vec<char>>;
+type Coords = (isize, isize);
+
+const DIRECTIONS: [Coords; 8] = [
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+];
+
+fn parse(input: &str) -> Result<Grid, AocError> {
+    let grid: Grid = input
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+
+    Ok(grid)
+}
+
+fn is_within_bounds(pos: Coords, width: isize, height: isize) -> bool {
+    pos.0 >= 0 && pos.1 >= 0 && pos.0 < width && pos.1 < height
+}
+
+fn xmas(x: usize, y: usize, (dx, dy): Coords, width: isize, height: isize, grid: &Grid) -> usize {
+    let mut pos = (x as isize, y as isize);
+
+    for current in ['M', 'A', 'S'] {
+        pos = (pos.0 + dx, pos.1 + dy);
+
+        if !is_within_bounds(pos, width, height) || grid[pos.1 as usize][pos.0 as usize] != current
+        {
+            return 0;
+        }
+    }
+
+    1
+}
+
+fn cross(x: usize, y: usize, width: isize, height: isize, grid: &Grid) -> usize {
+    let diagonals = [[(-1, 1), (1, -1)], [(-1, -1), (1, 1)]];
+
+    let valid = diagonals.iter().all(|[(dx1, dy1), (dx2, dy2)]| {
+        let first = (x as isize + dx1, y as isize + dy1);
+        let second = (x as isize + dx2, y as isize + dy2);
+
+        if !is_within_bounds(first, width, height) || !is_within_bounds(second, width, height) {
+            return false;
+        }
+
+        (grid[first.1 as usize][first.0 as usize] == 'M'
+            && grid[second.1 as usize][second.0 as usize] == 'S')
+            || (grid[first.1 as usize][first.0 as usize] == 'S'
+                && grid[second.1 as usize][second.0 as usize] == 'M')
+    });
+
+    if !valid {
+        return 0;
+    }
+
+    1
+}
+
+pub struct Day04;
+impl Solution for Day04 {
+    type A = usize;
+    type B = usize;
+
+    fn default_input(&self) -> &'static str {
+        include_str!("../../../inputs/2024/day04.txt")
+    }
+
+    fn part_1(&self, input: &str) -> Result<usize, AocError> {
+        let grid = parse(input)?;
+
+        let mut count = 0;
+
+        for y in 0..grid.len() {
+            for x in 0..grid[y].len() {
+                let width = grid[y].len() as isize;
+                let height = grid.len() as isize;
+
+                if grid[y][x] == 'X' {
+                    for direction in DIRECTIONS.into_iter() {
+                        count += xmas(x, y, direction, width, height, &grid);
+                    }
+                }
+            }
+        }
+
+        Ok(count)
+    }
+
+    fn part_2(&self, input: &str) -> Result<usize, AocError> {
+        let grid = parse(input)?;
+
+        let mut count = 0;
+
+        for y in 0..grid.len() {
+            for x in 0..grid[y].len() {
+                let width = grid[y].len() as isize;
+                let height = grid.len() as isize;
+
+                if grid[y][x] == 'A' {
+                    count += cross(x, y, width, height, &grid);
+                }
+            }
+        }
+
+        Ok(count)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example_1() {
+        assert_eq!(
+            Day04.part_1(
+                "..X...\n\
+                 .SAMX.\n\
+                 .A..A.\n\
+                 XMAS.S\n\
+                 .X...."
+            ),
+            Ok(4)
+        );
+    }
+
+    #[test]
+    fn it_solves_part1_example_2() {
+        assert_eq!(
+            Day04.part_1(
+                "MMMSXXMASM\n\
+                 MSAMXMSMSA\n\
+                 AMXSXMAAMM\n\
+                 MSAMASMSMX\n\
+                 XMASAMXAMM\n\
+                 XXAMMXXAMA\n\
+                 SMSMSASXSS\n\
+                 SAXAMASAAA\n\
+                 MAMMMXMMMM\n\
+                 MXMXAXMASX"
+            ),
+            Ok(18)
+        );
+    }
+
+    #[test]
+    fn it_solves_part2_example() {
+        assert_eq!(
+            Day04.part_2(
+                "MMMSXXMASM\n\
+                 MSAMXMSMSA\n\
+                 AMXSXMAAMM\n\
+                 MSAMASMSMX\n\
+                 XMASAMXAMM\n\
+                 XXAMMXXAMA\n\
+                 SMSMSASXSS\n\
+                 SAXAMASAAA\n\
+                 MAMMMXMMMM\n\
+                 MXMXAXMASX"
+            ),
+            Ok(9)
+        );
+    }
+}
--- aoc-solver/src/y2024/mod.rs
+++ aoc-solver/src/y2024/mod.rs
@@ -3,7 +3,7 @@ use crate::solution::{Solution, Solver};
 pub mod day01;
 pub mod day02;
 pub mod day03;
-// pub mod day04;
+pub mod day04;
 // pub mod day05;
 // pub mod day06;
 // pub mod day07;
@@ -26,7 +26,7 @@ pub mod day03;
 // pub mod day24;
 // pub mod day25;
 
-pub const MAX_DAYS: u8 = 3;
+pub const MAX_DAYS: u8 = 4;
 
 pub struct Y2024;
 
@@ -36,7 +36,7 @@ impl Solver for Y2024 {
             1 => day01::Day01.run(input, 1, 2024),
             2 => day02::Day02.run(input, 2, 2024),
             3 => day03::Day03.run(input, 3, 2024),
-            // 4 => day04::Day04.run(input, 4, 2024),
+            4 => day04::Day04.run(input, 4, 2024),
             // 5 => day05::Day05.run(input, 5, 2024),
             // 6 => day06::Day06.run(input, 6, 2024),
             // 7 => day07::Day07.run(input, 7, 2024),
@@ -78,7 +78,7 @@ impl Solver for Y2024 {
             1 => include_str!("./day01.rs"),
             2 => include_str!("./day02.rs"),
             3 => include_str!("./day03.rs"),
-            // 4 => include_str!("./day04.rs"),
+            4 => include_str!("./day04.rs"),
             // 5 => include_str!("./day05.rs"),
             // 6 => include_str!("./day06.rs"),
             // 7 => include_str!("./day07.rs"),
