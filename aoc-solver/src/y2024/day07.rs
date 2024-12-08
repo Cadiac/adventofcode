afbbe7ce9cc75b8283182acfc71475d895b17bec
Jaakko Husso <jaakko.t.husso@gmail.com>
2024-12-08 23:46:42 +0100

    Day 07 solutions @ Prague

---
+++ aoc-solver/src/y2024/day07.rs
@@ -0,0 +1,135 @@
+use itertools::Itertools;
+
+use crate::solution::{AocError, Solution};
+
+fn parse(input: &str) -> Result<Vec<(u64, Vec<u64>)>, AocError> {
+    let equations = input
+        .lines()
+        .map(|line| {
+            let (target, operands) = line
+                .split_once(": ")
+                .ok_or_else(|| AocError::parse(input, "Invalid input!"))?;
+
+            let target = target
+                .parse::<u64>()
+                .map_err(|err| AocError::parse(target, err))?;
+
+            let operands: Vec<u64> = operands
+                .split_ascii_whitespace()
+                .map(|i: &str| i.parse::<u64>().map_err(|err| AocError::parse(i, err)))
+                .try_collect()?;
+
+            Ok((target, operands))
+        })
+        .try_collect()?;
+
+    Ok(equations)
+}
+
+fn is_possible(target: u64, current: u64, rest: &[u64], concat: bool) -> bool {
+    if rest.is_empty() {
+        return current == target;
+    }
+
+    current < target && is_possible(target, current + rest[0], &rest[1..], concat)
+        || is_possible(target, current * rest[0], &rest[1..], concat)
+        || (concat
+            && is_possible(
+                target,
+                current * 10u64.pow(rest[0].ilog10() + 1) + rest[0],
+                &rest[1..],
+                concat,
+            ))
+}
+
+pub struct Day07;
+impl Solution for Day07 {
+    type A = u64;
+    type B = u64;
+
+    fn default_input(&self) -> &'static str {
+        include_str!("../../../inputs/2024/day07.txt")
+    }
+
+    fn part_1(&self, input: &str) -> Result<u64, AocError> {
+        let equations = parse(input)?;
+
+        let sum = equations
+            .into_iter()
+            .filter_map(|(result, operands)| {
+                is_possible(result, operands[0], &operands[1..], false).then_some(result)
+            })
+            .sum();
+
+        Ok(sum)
+    }
+
+    fn part_2(&self, input: &str) -> Result<u64, AocError> {
+        let equations = parse(input)?;
+
+        let sum = equations
+            .into_iter()
+            .filter_map(|(result, operands)| {
+                is_possible(result, operands[0], &operands[1..], true).then_some(result)
+            })
+            .sum();
+
+        Ok(sum)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example() {
+        assert_eq!(
+            Day07.part_1(
+                "190: 10 19\n\
+                 3267: 81 40 27\n\
+                 83: 17 5\n\
+                 156: 15 6\n\
+                 7290: 6 8 6 15\n\
+                 161011: 16 10 13\n\
+                 192: 17 8 14\n\
+                 21037: 9 7 18 13\n\
+                 292: 11 6 16 20"
+            ),
+            Ok(3749)
+        );
+    }
+
+    #[test]
+    fn it_solves_part2_example() {
+        assert_eq!(
+            Day07.part_2(
+                "190: 10 19\n\
+                 3267: 81 40 27\n\
+                 83: 17 5\n\
+                 156: 15 6\n\
+                 7290: 6 8 6 15\n\
+                 161011: 16 10 13\n\
+                 192: 17 8 14\n\
+                 21037: 9 7 18 13\n\
+                 292: 11 6 16 20"
+            ),
+            Ok(11387)
+        );
+    }
+
+    #[test]
+    fn it_solves_part2_example_1() {
+        assert_eq!(Day07.part_2("156: 15 6"), Ok(156));
+    }
+
+    #[test]
+    fn it_solves_part2_example_2() {
+        assert_eq!(Day07.part_2("7290: 6 8 6 15"), Ok(7290));
+    }
+
+    #[test]
+    fn it_solves_part2_example_3() {
+        assert_eq!(Day07.part_2("192: 17 8 14"), Ok(192));
+    }
+}
--- aoc-solver/src/y2024/mod.rs
+++ aoc-solver/src/y2024/mod.rs
@@ -6,7 +6,7 @@ pub mod day03;
 pub mod day04;
 pub mod day05;
 pub mod day06;
-// pub mod day07;
+pub mod day07;
 // pub mod day08;
 // pub mod day09;
 // pub mod day10;
@@ -26,7 +26,7 @@ pub mod day06;
 // pub mod day24;
 // pub mod day25;
 
-pub const MAX_DAYS: u8 = 5;
+pub const MAX_DAYS: u8 = 7;
 
 pub struct Y2024;
 
@@ -39,7 +39,7 @@ impl Solver for Y2024 {
             4 => day04::Day04.run(input, 4, 2024),
             5 => day05::Day05.run(input, 5, 2024),
             6 => day06::Day06.run(input, 6, 2024),
-            // 7 => day07::Day07.run(input, 7, 2024),
+            7 => day07::Day07.run(input, 7, 2024),
             // 8 => day08::Day08.run(input, 8, 2024),
             // 9 => day09::Day09.run(input, 9, 2024),
             // 10 => day10::Day10.run(input, 10, 2024),
