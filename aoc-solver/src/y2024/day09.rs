77e17828c39f6c9ee03e77724b5ddb4d2873542b
Jaakko Husso <jaakko.t.husso@gmail.com>
2024-12-09 21:18:40 +0100

    Day 09 part 1 on an airplane, probably works but can't run it..

---
+++ aoc-solver/src/y2024/day09.rs
@@ -0,0 +1,194 @@
+use itertools::Itertools;
+
+use crate::solution::{AocError, Solution};
+
+type Id = u32;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Block {
+    Free,
+    File(Id),
+}
+
+fn parse(input: &str) -> Result<Vec<Block>, AocError> {
+    let mut is_parsing_file = true;
+    let mut id = 0;
+
+    let mut blocks = Vec::new();
+    for character in input.chars() {
+        let size = character
+            .to_digit(10)
+            .ok_or_else(|| AocError::parse(character.to_string(), "Unexpected character"))?;
+
+        if is_parsing_file {
+            for _ in 0..size {
+                blocks.push(Block::File(id));
+            }
+            id += 1;
+            is_parsing_file = false;
+        } else {
+            for _ in 0..size {
+                blocks.push(Block::Free);
+            }
+            is_parsing_file = true;
+        }
+    }
+
+    Ok(blocks)
+}
+
+fn compact(blocks: &mut [Block]) {
+    let mut head = 0;
+    let mut tail = blocks.len() - 1;
+
+    loop {
+        while let Some(Block::File(_)) = blocks.get(head) {
+            head += 1
+        }
+
+        while let Some(Block::Free) = blocks.get(tail) {
+            tail -= 1
+        }
+
+        if head >= tail {
+            return;
+        }
+
+        blocks.swap(head, tail);
+    }
+}
+
+fn checksum(blocks: &[Block]) -> u32 {
+    let mut sum = 0;
+
+    for (index, block) in blocks.iter().enumerate() {
+        if let Block::File(id) = block {
+            sum += index as u32 * id
+        }
+    }
+
+    sum
+}
+
+pub struct Day09;
+impl Solution for Day09 {
+    type A = u32;
+    type B = u32;
+
+    fn default_input(&self) -> &'static str {
+        include_str!("../../../inputs/2024/day09.txt")
+    }
+
+    fn part_1(&self, input: &str) -> Result<u32, AocError> {
+        let mut disk = parse(input)?;
+
+        compact(&mut disk);
+
+        Ok(checksum(&disk))
+    }
+
+    fn part_2(&self, input: &str) -> Result<u32, AocError> {
+        unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example() {
+        assert_eq!(Day09.part_1("2333133121414131402"), Ok(1928));
+    }
+
+    #[test]
+    fn it_should_parse() {
+        assert_eq!(
+            parse("12345"),
+            Ok(vec![
+                Block::File(0),
+                Block::Free,
+                Block::Free,
+                Block::File(1),
+                Block::File(1),
+                Block::File(1),
+                Block::Free,
+                Block::Free,
+                Block::Free,
+                Block::Free,
+                Block::File(2),
+                Block::File(2),
+                Block::File(2),
+                Block::File(2),
+                Block::File(2),
+            ])
+        );
+    }
+
+    #[test]
+    fn it_should_compact() {
+        let mut disk = [
+            Block::File(0),
+            Block::Free,
+            Block::Free,
+            Block::File(1),
+            Block::File(1),
+            Block::File(1),
+            Block::Free,
+            Block::Free,
+            Block::Free,
+            Block::Free,
+            Block::File(2),
+            Block::File(2),
+            Block::File(2),
+            Block::File(2),
+            Block::File(2),
+        ];
+
+        compact(&mut disk);
+
+        assert_eq!(
+            disk,
+            [
+                Block::File(0),
+                Block::File(2),
+                Block::File(2),
+                Block::File(1),
+                Block::File(1),
+                Block::File(1),
+                Block::File(2),
+                Block::File(2),
+                Block::File(2),
+                Block::Free,
+                Block::Free,
+                Block::Free,
+                Block::Free,
+                Block::Free,
+                Block::Free,
+            ]
+        );
+    }
+
+    #[test]
+    fn it_should_calculate_checkshum() {
+        let disk = [
+            Block::File(0),
+            Block::File(2),
+            Block::File(2),
+            Block::File(1),
+            Block::File(1),
+            Block::File(1),
+            Block::File(2),
+            Block::File(2),
+            Block::File(2),
+            Block::Free,
+            Block::Free,
+            Block::Free,
+            Block::Free,
+            Block::Free,
+            Block::Free,
+        ];
+
+        assert_eq!(checksum(&disk), 60);
+    }
+}
--- aoc-solver/src/y2024/mod.rs
+++ aoc-solver/src/y2024/mod.rs
@@ -8,7 +8,7 @@ pub mod day05;
 pub mod day06;
 pub mod day07;
 pub mod day08;
-// pub mod day09;
+pub mod day09;
 // pub mod day10;
 // pub mod day11;
 // pub mod day12;
@@ -26,7 +26,7 @@ pub mod day08;
 // pub mod day24;
 // pub mod day25;
 
-pub const MAX_DAYS: u8 = 7;
+pub const MAX_DAYS: u8 = 9;
 
 pub struct Y2024;
 
@@ -41,7 +41,7 @@ impl Solver for Y2024 {
             6 => day06::Day06.run(input, 6, 2024),
             7 => day07::Day07.run(input, 7, 2024),
             8 => day08::Day08.run(input, 8, 2024),
-            // 9 => day09::Day09.run(input, 9, 2024),
+            9 => day09::Day09.run(input, 9, 2024),
             // 10 => day10::Day10.run(input, 10, 2024),
             // 11 => day11::Day11.run(input, 11, 2024),
             // 12 => day12::Day12.run(input, 12, 2024),
